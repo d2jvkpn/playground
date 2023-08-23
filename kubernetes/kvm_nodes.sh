2eab2f146fac5f76ede0accbb6b232ce26ab29f7
d2jvkpn <d2jvkpn@noreply.local>
2023-08-23 09:51:13 +0800

    ...

--- kubernetes/kvm_nodes.sh
+++ kubernetes/kvm_nodes.sh
@@ -23,10 +23,12 @@ EOF
 #   sed 's#\x27##g; s#/>##; s#name=##; s#ip=##' > configs/etc_hosts
 
 virsh net-dumpxml $KVM_Network |
-  awk "/<host.*name='node/{print} /<host.*name='cp/{print}" |
+  awk "/<host.*name='node/{print} /<host.*name='cp/{print} /<host.*name='ingress/{print}" |
   sed "s#^.*name='##; s#ip='##; s#/>##; s#'##g" |
-  awk '{print $2, $1}' > configs/etc_hosts
+  awk 'BEGIN{OFS="\t"; print "hostname", "ip"} {print $1, $2}' > configs/hosts.tsv
 
-awk '{print $2, "ansible_host="$1}' configs/etc_hosts |
-  sed 's#$# ansible_port=22 ansible_user=ubuntu#' |
-  sed '1i[k8s_all]' > configs/hosts.ini
+awk 'BEGIN{print "[k8s_all]"} NR>1{
+    $2="ansible_host="$2;
+    $0=$0"  ansible_port=22 ansible_user=ubuntu";
+    print;
+  }' configs/hosts.tsv > configs/hosts.ini
