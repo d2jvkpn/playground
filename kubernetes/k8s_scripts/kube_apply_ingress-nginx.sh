5c38b4cd9056178623ec7cfa844c81115f427570
d2jvkpn <d2jvkpn@noreply.local>
2023-08-30 19:49:00 +0800

    k8s deployemnt: tolerations and affinity

--- kubernetes/k8s_scripts/kube_apply_ingress-nginx.sh
+++ kubernetes/k8s_scripts/kube_apply_ingress-nginx.sh
@@ -3,44 +3,51 @@ set -eu -o pipefail
 _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
-# ingress_node=node01 # an worker node
+# ingress_node=k8s-ingress01
 ingress_node=$1
 
 mkdir -p k8s_data
 
-ip=$(kubectl get node/$ingress_node -o wide | awk 'NR==2{print $6}')
+node_ip=$(kubectl get node/$ingress_node -o wide | awk 'NR==2{print $6}')
 
-#### by lable node
-# kubectl label nodes/$ingress_node ingress-ready=yes
+# https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 
-# # https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+kubectl label nodes/$ingress_node node-type=ingress
+# kubectl label nodes/$ingress_node node-type-
+
+kubectl taint nodes $ingress_node node-type=ingress:NoSchedule
+# kubectl taint nodes $ingress_node node-type=ingress:NoSchedule-
+
+# kubectl get nodes/$ingress_node -o yaml
+
 # sed '/image:/s/@sha256:.*//' k8s_apps/ingress-nginx_cloud.yaml |
 #   awk 'BEGIN{RS=ORS="---"}
-#   /Deployment/{sub("nodeSelector:", "nodeSelector:\n        ingress-ready: \"yes\"")}
+#   /Deployment/{sub("nodeSelector:", "nodeSelector:\n        node-type: ingress")}
 #   { print }' > k8s_data/ingress-nginx.yaml
 
-#### by taint node
-kubectl taint nodes $ingress_node node-role.k8s.local/ingress=:NoSchedule
-# kubectl taint nodes $ingress_node node-role.k8s.local/ingress=:NoSchedule-
-
-# tolerations='tolerations: [{key: "node-role.k8s.local/ingress", value: "", effect: "NoSchedule"}]'
-
-tolerations='''
+fields='''
       tolerations:
-      - { key: "node-role.k8s.local/ingress", value: "", effect: "NoSchedule" }
+      - { key: node-type, value: ingress, operator: Equal, effect: NoSchedule }
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+              - matchExpressions:
+                - { key: node-type, operator: In, values: [ingress] }
 '''
 
 sed '/image:/s/@sha256:.*//' k8s_apps/ingress-nginx_cloud.yaml |
-  awk -v tolerations="$tolerations" 'BEGIN{RS=ORS="---"}
-  /Deployment/{$0=$0""tolerations}
-  {print}'> k8s_data/ingress-nginx.yaml
+  awk -v fields="$fields" 'BEGIN{RS=ORS="---"}
+  /Deployment/{$0=$0""fields} {print}' |
+  yq --prettyPrint > k8s_data/ingress-nginx.yaml
 
 kubectl apply -f k8s_data/ingress-nginx.yaml
-# kubectl delete -f k8s_apps/ingress-nginx.yaml
+# kubectl get pods -A -o wide
+# kubectl delete -f k8s_data/ingress-nginx.yaml
 
 kubectl -n ingress-nginx patch svc/ingress-nginx-controller \
-  -p "$(printf '{"spec":{"externalIPs":["%s"]}}' $ip)"
-# '{"spec":{"externalIPs":["'$ip'"]}}'
+  -p "$(printf '{"spec":{"externalIPs":["%s"]}}' $node_ip)"
+# -p '{"spec":{"externalIPs":["'$ip'"]}}'
 
 kubectl -n ingress-nginx get deploy
 kubectl -n ingress-nginx get pods --field-selector status.phase=Running -o wide
@@ -58,4 +65,4 @@ kubectl -n ingress-nginx describe pod
 # ip=$(hostname -I | awk '{print $1; exit}')
 
 ####
-curl -i -H "Host: Your.Domain" $ip
+curl -i -H "Host: Your.Domain" $node_ip
