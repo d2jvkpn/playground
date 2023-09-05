c5e6901b1acfb2316bdd95636e3f5ae7cc9a84b3
d2jvkpn <d2jvkpn@noreply.local>
2023-09-05 19:22:44 +0800

    ...

--- demo-web/k8s/k8s_do.sh
+++ demo-web/k8s/k8s_do.sh
@@ -3,10 +3,19 @@ set -eu -o pipefail
 _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
-kubectl -n dev create configmap demo-web --from-file=config.dev.yaml
+kubectl config set-context --current --namespace=dev
+
+sudo mkdir -p /data/local/demo-web
+sudo chmod -R 777 /data/local
+
+# kubectl -n dev create configmap demo-web --from-file=config.dev.yaml
+
+kubectl create configmap demo-web --from-file=config.dev.yaml
 
 kubectl apply -f deploy.yaml
 
-kubectl -n dev get deploy/demo-web
+kubectl get deploy/demo-web
+
+kubectl describe deploy/demo-web
 
 kubectl scale --replicas=2 deploy/demo-web
