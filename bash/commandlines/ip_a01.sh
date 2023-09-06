5b70ad28d117eb6cdcaa0ea1d8ff90aa6f1f7e66
d2jvkpn <d2jvkpn@noreply.local>
2023-09-06 12:27:07 +0800

    ...

---
+++ bash/commandlines/ip_a01.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+ip route show default
+
+dev=$(ip route show default | awk '{print $5}')
+ip -4 addr show $dev | awk '/inet/{print $2}'
