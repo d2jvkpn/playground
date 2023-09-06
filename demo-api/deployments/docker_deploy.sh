7f64a6683f180e2d2afec210901f49562a0b253b
d2jvkpn <d2jvkpn@noreply.local>
2023-09-06 21:43:53 +0800

    ...

--- demo-api/deployments/docker_deploy.sh
+++ demo-api/deployments/docker_deploy.sh
@@ -3,14 +3,14 @@ set -eu -o pipefail
 _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
-name=demo-api
-mkdir -p configs logs data
-
 export TAG=$1 APP_ENV=$2 PORT=$3
+app=$(yq .app project.yaml)
+
+mkdir -p configs logs data
 envsubst < ${_path}/docker_deploy.yaml > docker-compose.yaml
 
 # docker-compose pull
-[[ ! -z "$(docker ps --all --quiet --filter name=${name}_$APP_ENV)" ]] &&
+[ ! -z "$(docker ps --all --quiet --filter name=${app}_${APP_ENV})" ] &&
   docker rm -f ${name}_$APP_ENV
 # docker-compose down for running containers only, not stopped containers
 
