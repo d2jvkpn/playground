08163cdd85511a63d9dc64636da0330624aa3318
d2jvkpn <d2jvkpn@noreply.local>
2023-09-06 13:23:08 +0800

    deployments

--- demo-web/deployments/k8s_do.sh
+++ demo-web/deployments/k8s_do.sh
@@ -8,17 +8,17 @@ ansible k8s-cp01 -m copy -a 'src=../demo-web dest=./'
 ansible k8s_workers -m shell -a 'sudo mkdir -p /data/logs && sudo chmod -R 777 /data/logs'
 
 ssh k8s-cp01
-cd demo-web/k8s
+cd demo-web
 
 #### deployment
 kubectl config set-context --current --namespace=dev
 # kubectl config view --minify -o jsonpath='{..namespace}'
 kubectl config view | grep namespace
 
-# kubectl -n dev create configmap demo-web --from-file=dev.yaml
-kubectl create configmap demo-web --from-file=dev.yaml
+# kubectl -n dev create configmap demo-web --from-file=deployments/dev.yaml
+kubectl create configmap demo-web --from-file=deployments/dev.yaml
 
-kubectl apply -f deploy.yaml
+kubectl apply -f deployments/deploy.yaml
 kubectl get pods | awk '/^demo-web-/{print $1; exit}' | xargs -i kubectl describe pod/{}
 
 kubectl get pods | awk 'NR>1{print $1}' | xargs -i kubectl logs pod/{}
@@ -33,8 +33,8 @@ kubectl get pod/$pod -o wide
 kubectl exec -it $pod -- sh
 
 #### services and ingress-http
-kubectl apply -f cluster-ip.yaml
-kubectl apply -f ingress.http.yaml
+kubectl apply -f deployments/k8s_cluster-ip.yaml
+kubectl apply -f deployments/ingress.http.yaml
 
 # curl -H 'Host: demo-web.dev.noreply.local' k8s-ingress01/api/v1/open/hello
 # curl -H 'Host: demo-web.dev.noreply.local' k8s-ingress01/api/v1/open/world
@@ -46,17 +46,13 @@ kubectl create secret tls noreply.local --key noreply.local.key --cert noreply.l
 kubectl -n prod get secret/noreply.local
 
 #### create secret and ingress-https
-_update_cert="""
 kubectl create secret tls noreply.local \
   --key noreply.local.key --cert noreply.local.cer -o yaml --dry-run=client |
   kubectl apply -f -
-"""
 
-_registry_cert="""
 kubectl create secret docker-registry noreply.local \
   --docker-server=registry.noreply.local --docker-email=EMAIL \
   --docker-username=USERNAME --docker-password=PASSWORD
-"""
 
 # curl -k -H 'Host: demo-web.dev.noreply.local' https://k8s-ingress01/api/v1/open/hello
 # curl -H 'Host: demo-web.dev.noreply.local' https://k8s-ingress01/api/v1/open/world
