b1c3f82598835ca0e5e829499296a59a29023db8
d2jvkpn <d2jvkpn@noreply.local>
2023-09-07 14:03:29 +0800

    ...

--- bash/prototype/_recursion.sh
+++ bash/prototype/_recursion.sh
@@ -3,8 +3,9 @@ set -eu -o pipefail
 _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
+
 if [ $# -eq 0 ]; then
-    >&2 echo "arg(s) not provided"
+    >&2 echo "arg(s) not provided!"
     exit 1
 elif [ $# -eq 1 ]; then
     arg=$1
@@ -16,5 +17,7 @@ else
 fi
 
 >&2 echo "[$(date +%FT%T.%N%:z)] ==> arg: $arg"
+
 sleep 0.42
+
 #### more codes...
