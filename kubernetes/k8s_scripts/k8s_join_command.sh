acb32714201fd2470e3505b3e0ada5514de1714b
d2jvkpn <d2jvkpn@noreply.local>
2023-09-10 09:38:56 +0800

    ...

--- kubernetes/k8s_scripts/k8s_join_command.sh
+++ kubernetes/k8s_scripts/k8s_join_command.sh
@@ -5,10 +5,12 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 node_kind=$1
 
-cp_endpoint=$(yq .cp_endpoint k8s_data/kubeadm-init.yaml)
-token=$(yq .token k8s_data/kubeadm-init.yaml)
-cert_hash=$(yq .cert_hash k8s_data/kubeadm-init.yaml)
-cert_key=$(yq .cert_key k8s_data/kubeadm-init.yaml)
+yaml_file=${yaml_file:-k8s_data/kubeadm-init.yaml}
+
+cp_endpoint=$(yq .cp_endpoint $yaml_file)
+token=$(yq .token $yaml_file)
+cert_hash=$(yq .cert_hash $yaml_file)
+cert_key=$(yq .cert_key $yaml_file)
 
 cmd="kubeadm join $cp_endpoint --token ${token} --discovery-token-ca-cert-hash ${cert_hash}"
 
