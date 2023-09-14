fb79b1a0bca3a6b66b04b58d7898784228a1d884
d2jvkpn <d2jvkpn@noreply.local>
2023-09-14 11:12:29 +0800

    ...

--- kubernetes/k8s_scripts/k8s_apps_downloads.sh
+++ kubernetes/k8s_scripts/k8s_apps_downloads.sh
@@ -75,12 +75,6 @@ EOF
 download_images k8s_apps/k8s.yaml k8s_apps/images
 
 exit
-yq_dir=k8s_apps/yq-${yq_version}-linux-amd64
-mkdir -p $yq_dir
-
-tar -xf k8s_apps/yq_linux_amd64.tar.gz -C $yq_dir
-tar -C k8s_apps -czf $yq_dir.tar.gz yq-${yq_version}-linux-amd64
-rm -rf $yq_dir k8s_apps/yq_linux_amd64.tar.gz
 
 #### 5. nerdctl
 nerdctl_version=${nerdctl_version:-1.5.0}
