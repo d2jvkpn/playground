d64db2e40815ddcc070f441ca4b86beb604459a3
d2jvkpn <d2jvkpn@noreply.local>
2023-09-19 09:48:40 +0800

    ...

--- kvm/src/virsh_archive.sh
+++ kvm/src/virsh_archive.sh
@@ -4,18 +4,24 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
 op=$1
-username=$(whoami)
+# username=$(whoami)
+
+if [ $(id -u) -ne 0 ]; then
+    >&2 echo "Please run as root"
+    exit 1
+fi
+
+mkdir -p data
 
 if [[ "$op" == "backup" ]]; then
     target=$2
     out_zip=data/${target}_kvm_$(date +%FT%H-%M-%S.%N).zip
     echo "==> backup $target to $out_zip"
-    mkdir -p data
 
     virsh shutdown $target || true
     virsh dumpxml $target > data/$target.kvm.xml
-    sudo qemu-img convert -O raw /var/lib/libvirt/images/$target.qcow2 data/$target.kvm.raw
-    sudo chown $username:$username data/$target.kvm.raw
+    qemu-img convert -O raw /var/lib/libvirt/images/$target.qcow2 data/$target.kvm.raw
+    # sudo chown $username:$username data/$target.kvm.raw
 
     zip -j -r $out_zip data/$target.kvm.xml data/$target.kvm.raw
     rm -f data/$target.kvm.xml data/$target.kvm.raw
@@ -23,20 +29,22 @@ if [[ "$op" == "backup" ]]; then
 elif [[ "$op" == "restore" ]]; then
     zip_file=$2
     target=$(basename $zip_file | sed 's/_kvm_[1-9]*.*//')
-    mkdir -p data
+
     unzip $zip_file -d data
     echo "==> restore $target"
     ls data/$target.kvm.xml data/$target.kvm.raw > /dev/null
 
     virsh define data/$target.kvm.xml
-    sudo qemu-img convert -O qcow2 data/$target.kvm.raw /var/lib/libvirt/images/$target.qcow2
+    qemu-img convert -O qcow2 data/$target.kvm.raw /var/lib/libvirt/images/$target.qcow2
     rm -f data/$target.kvm.xml data/$target.kvm.raw
 else
     >&2 echo "invalid operation"
     exit 1
 fi
 
-# virt-clone --original ubuntu --name ubuntu-node --file ubuntu-node.qcow2
+exit 0
 
-# virt-install --name ubuntu --import --os-variant=generic --network default --nograph \
-#   --memory 2048 --vcpus 2 --disk /var/lib/libvirt/images/ubuntu-node.qcow2,bus=sata
+virt-clone --original ubuntu --name ubuntu-node --file ubuntu-node.qcow2
+
+virt-install --name ubuntu --import --os-variant=generic --network default --nograph \
+  --memory 2048 --vcpus 2 --disk /var/lib/libvirt/images/ubuntu-node.qcow2,bus=sata
