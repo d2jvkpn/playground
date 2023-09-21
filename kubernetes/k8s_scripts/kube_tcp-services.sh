6d3f0bd37fe5b1288107d6ba41e7bf16dbf4b822
d2jvkpn <d2jvkpn@noreply.local>
2023-09-21 19:51:21 +0800

    update kube_tcp-services.sh

--- kubernetes/k8s_scripts/kube_tcp-services.sh
+++ kubernetes/k8s_scripts/kube_tcp-services.sh
@@ -5,6 +5,10 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 # https://kubernetes.github.io/ingress-nginx/user-guide/exposing-tcp-udp-services/*
 
+namespace=${namespace:-dev}
+srv=$1
+port=$2
+
 kubectl -n ingress-nginx get deploy/ingress-nginx-controller -o yaml |
   yq eval '.spec.template.spec.containers[0].args'
 
@@ -24,13 +28,49 @@ kubectl -n ingress-nginx get deploy/ingress-nginx-controller -o yaml |
 #   yq eval '.spec.template.spec.containers[0].args += ["--udp-services-configmap=$(POD_NAMESPACE)/udp-services"]' |
 #   kubectl apply -f -
 
+found=$(
+  kubectl -n ingress-nginx get services/ingress-nginx-controller -o yaml |
+    yq eval .spec.ports |
+    grep -w $port
+)
+
+[ -z "$found" ] &&
+kubectl -n ingress-nginx get services/ingress-nginx-controller -o yaml |
+  yq eval '.spec.ports += [{"name":"'$srv'","protocol":"TCP","port":'$port',"targetPort":'$port'}]' |
+  kubectl apply -f -
+
+kubectl -n ingress-nginx get services/ingress-nginx-controller
+
+found=$(kubectl -n ingress-nginx get cm/tcp-services || true)
+
+[ -z "$found" ] &&
+cat | kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: ingress-nginx
+  name: tcp-services
+data:
+#  "5432": dev/postgres:5432
+EOF
+
+kubectl -n ingress-nginx get cm/tcp-services -o yaml |
+  yq eval '.data += {"'$port'": "'$namespace/$srv:$port'"}' |
+  kubectl apply -f -
+
+kubectl -n ingress-nginx get cm/tcp-services -o yaml
+
+exit
+
+# psql --host k8s.local --username postgres --port 5432
+
 postgres_port=$(
   kubectl -n ingress-nginx get services/ingress-nginx-controller -o yaml |
     yq eval .spec.ports |
     grep -w 5432
 )
 
-[ -z "$postgres_port" ] && 
+[ -z "$postgres_port" ] &&
 kubectl -n ingress-nginx get services/ingress-nginx-controller -o yaml |
   yq eval '.spec.ports += [{"name":"postgres","protocol":"TCP","port":5432,"targetPort":5432}]' |
   kubectl apply -f -
@@ -45,20 +85,3 @@ mysql_port=$(
 kubectl -n ingress-nginx get services/ingress-nginx-controller -o yaml |
   yq eval '.spec.ports += [{"name":"mysql","protocol":"TCP","port":3306,"targetPort":3306}]' |
   kubectl apply -f -
-
-kubectl -n ingress-nginx get services/ingress-nginx-controller
-
-cat | kubectl apply -f - <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  namespace: ingress-nginx
-  name: tcp-services
-data:
-  "5432": dev/postgres:5432
-  "3306": dev/mysql:3306
-EOF
-
-kubectl -n ingress-nginx get cm/tcp-services -o yaml
-
-# psql --host k8s.local --username postgres --port 5432
