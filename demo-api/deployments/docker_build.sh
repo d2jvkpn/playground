2c354df838c0c68c723ebc29e12d870db87f3e81
d2jvkpn <d2jvkpn@noreply.local>
2023-10-08 01:00:43 +0800

    ...

--- demo-api/deployments/docker_build.sh
+++ demo-api/deployments/docker_build.sh
@@ -23,7 +23,10 @@ DOCKER_Pull=${DOCKER_Pull:-"true"}
 DOCKER_Push=${DOCKER_Push:-"true"}
 BUILD_Region=${BUILD_Region:-""}
 
-[ -f .env ] && . .env
+[ -f .env ] && {
+  2>&1 echo "==> load .env"
+  . .env
+}
 
 #### git
 function on_exit() {
