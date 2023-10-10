081e767381dc27cf9360f73b44862d644187c10c
d2jvkpn <d2jvkpn@noreply.local>
2023-10-10 18:51:38 +0800

    ...

--- docker/stable-diffusion-webui/entrypoint.sh
+++ docker/stable-diffusion-webui/entrypoint.sh
@@ -9,7 +9,9 @@ python3 -m venv venv/
 python3 launch.py $*
 
 # --listen --api --port=7860
-# --nowebui(no webui), --medvram, --no-download-sd-model, --skip-install
-# --disable-safe-unpickle, --no-download-sd-model
+# -xformers --ckpt=models/Stable-diffusion/realisticVisionV13_v13.safetensors
+# --nowebui --medvram --no-download-sd-model --skip-install
+# --disable-safe-unpickle --no-download-sd-model
+
 # address: http://localhost:7860/?__theme=dark
 # curl http://localhost:7860
