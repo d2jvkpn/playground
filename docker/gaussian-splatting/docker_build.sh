a3fa8369dea832297fdd8b02a946be5a64ed6f9b
d2jvkpn <d2jvkpn@noreply.local>
2023-10-12 19:22:03 +0800

    ...

--- docker/gaussian-splatting/docker_build.sh
+++ docker/gaussian-splatting/docker_build.sh
@@ -5,34 +5,46 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 container=gaussian-splatting_$(tr -dc '0-9a-z' < /dev/urandom | fold -w 8 | head -n 1 || true)
 
-# docker build -f Dockerfile -t gaussian-splatting ./
-docker run -d --name $container --gpus=all nvidia/cuda:11.7.1-devel-ubuntu22.04 tail -f /etc/hosts
+{
+    echo "==> $(date +'%FT%T%:z') docker build start"
+    # docker build -f Dockerfile -t gaussian-splatting ./
+    docker run -d --name $container --gpus=all nvidia/cuda:11.7.1-devel-ubuntu22.04 \
+      tail -f /etc/hosts
 
-## not working:
-# docker run -tid --name gaussian-splatting --gpus=all nvidia/cuda:12.2.0-devel-ubuntu22.04 bash
+    ## not working:
+    # docker run -tid --name gaussian-splatting --gpus=all nvidia/cuda:12.2.0-devel-ubuntu22.04 bash
 
-docker exec $container mkdir -p /opt/bin
+    docker exec $container mkdir -p /opt/bin
 
-docker cp ./build_app.sh $container:/opt/build_app.sh
-docker cp ./conda_app.sh $container:/opt/bin/conda_app.sh
+    docker cp ./build_app.sh $container:/opt/build_app.sh
+    docker cp ./conda_app.sh $container:/opt/bin/conda_app.sh
 
-docker exec $container chmod a+x /opt/bin/conda_app.sh
-docker exec $container bash /opt/build_app.sh
+    docker exec $container chmod a+x /opt/bin/conda_app.sh
+    docker exec $container bash /opt/build_app.sh
 
-# docker commit -p --change='ENTRYPOINT ["/entrypoint.sh"]' $container gaussian-splatting:latest
-docker commit -p \
-  --change='ENV TZ=Asia/Shanghai' \
-  --change='ENV CONDA_DIR=/opt/conda' \
-  --change='ENV PATH=/opt/bin:$CONDA_DIR/bin:$PATH' \
-  --change='ENV PATH=/opt/gaussian-splatting/SIBR_viewers/install/bin:$PATH' \
-  --change='WORKDIR /opt/gaussian-splatting' \
-  $container gaussian-splatting:latest
+    # docker commit -p --change='ENTRYPOINT ["/entrypoint.sh"]' $container gaussian-splatting:latest
+    docker commit -p \
+      --change='ENV TZ=Asia/Shanghai' \
+      --change='ENV CONDA_DIR=/opt/conda' \
+      --change='ENV PATH=/opt/bin:$CONDA_DIR/bin:$PATH' \
+      --change='ENV PATH=/opt/gaussian-splatting/SIBR_viewers/install/bin:$PATH' \
+      --change='WORKDIR /opt/gaussian-splatting' \
+      $container gaussian-splatting:latest
 
-docker stop $container && docker rm $container
+    docker stop $container && docker rm $container
 
-exit
+    echo "==> $(date +'%FT%T%:z') docker build end"
+} > docker_gaussian-splatting.$(date +'%FT%H-%M-%S').log
+
 docker save gaussian-splatting:latest -o gaussian-splatting_latest.tar
 pigz gaussian-splatting_latest.tar
+
+exit
+
+aws s3 cp ./gaussian-splatting_latest.tar.gz s3://$bucket/tests/
+aws s3 ls --recursive s3://$bucket/tests
+aws s3 presign s3://$bucket/tests/gaussian-splatting_latest.tar.gz
+aws s3 rm s3://$bucket/tests/gaussian-splatting_latest.tar.gz
 
 docker run --rm -it --gpus=all gaussian-splatting:latest bash
 
