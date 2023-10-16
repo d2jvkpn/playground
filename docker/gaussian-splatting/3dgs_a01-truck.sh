efd292025b62820eb9249c2e9fe8846f0fb05b67
d2jvkpn <d2jvkpn@noreply.local>
2023-10-16 14:43:33 +0800

    ...

--- docker/gaussian-splatting/3dgs_a01-truck.sh
+++ docker/gaussian-splatting/3dgs_a01-truck.sh
@@ -7,11 +7,11 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 # sudo apt install -y colmap
 # colmap gui
 
-# input: ./truck/images/*.png
+# input: ./truck_1/images/*.png
 # output: distorted/database.db, distorted/sparse/
-mkdir -p ./truck/distorted
-colmap automatic_reconstructor --image_path ./truck/images --workspace_path ./truck/distorted
-ls -d truck/input truck/distorted/{database.db,sparse}
+mkdir -p ./truck_1/distorted
+colmap automatic_reconstructor --image_path ./truck_1/images --workspace_path ./truck_1/distorted
+ls -d truck_1/input truck_1/distorted/{database.db,sparse}
 
 #### 3dgs container
 docker run -d --name 3dgs --gpus=all -v $PWD/truck:/home/d2jvkpn/3dgs_workspace \
@@ -27,7 +27,7 @@ conda activate gaussian_splatting
 # input: input/*.png distorted/database.db distorted/sparse/0
 # output: images_2/, images_4/, images_8/, sparse/, stereo/, run-colmap-geometric.sh, run-colmap-photometric.sh
 ln -s images input
-# python ../3dgs/convert.py -s ./truck --skip_matching
+# python ../3dgs/convert.py -s ./ --skip_matching
 python ../3dgs/convert.py -s ./ --skip_matching --resize --magick_executable /usr/bin/convert
 
 #### train.py
