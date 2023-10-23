392acb2829c64464eb301e18ee3905f9bdae4bd6
d2jvkpn <d2jvkpn@noreply.local>
2023-10-23 22:31:14 +0800

    ...

--- docker/gaussian-splatting/docker_build.sh
+++ docker/gaussian-splatting/docker_build.sh
@@ -11,10 +11,11 @@ docker run -d --name $container --gpus=all nvidia/cuda:11.8.0-devel-ubuntu22.04 
 function remove_container() {
     docker rm -f $container
 }
-trap 'remove_container' ERR
+# trap 'remove_container' ERR
+trap 'remove_container' EXIT
 
 {
-    echo "==> $(date +'%FT%T%:z') docker build start"
+    date +'==> %FT%T%:z docker build start'
 
     docker exec $container mkdir -p /home/d2jvkpn/3dgs_workspace
     docker cp ./3dgs_install.sh $container:/home/d2jvkpn/
@@ -29,19 +30,19 @@ trap 'remove_container' ERR
       --change='ENV CONDA_HOME=/opt/conda' \
       --change='ENV PATH=/opt/gaussian-splatting/SIBR_viewers/install/bin:$CONDA_HOME/bin:$PATH' \
       --change='WORKDIR /home/d2jvkpn/3dgs_workspace' \
-      # --change='ENTRYPOINT ["bash", "/home/d2jvkpn/3dgs_pipeline.sh"]' \
       $container 3dgs:latest
+      # --change='ENTRYPOINT ["bash", "/home/d2jvkpn/3dgs_pipeline.sh"]' \
 
-    docker stop $container && docker rm $container
-
-    echo "==> $(date +'%FT%T%:z') docker build end"
+    date +'==> %FT%T%:z docker build end'
 } &> 3dgs.$(date +'%FT%H-%M-%S').log
 
+docker stop $container && docker rm $container
 docker save 3dgs:latest -o 3dgs_latest.tar && pigz -f 3dgs_latest.tar
 # docker rmi 3dgs:latest
 
 exit
 
+####
 [ ! -z "${AWS_Bucket:-""}" ] && aws s3 cp ./3dgs_latest.tar.gz s3://${AWS_Bucket}/tests/
 [ ! -z "${EXIT_Command:=""}" ] && $EXIT_Command
 
@@ -51,22 +52,11 @@ aws s3 rm s3://${AWS_Bucket}/tests/3dgs_latest.tar.gz
 
 ####
 docker run --name 3dgs -d --gpus=all 3dgs:latest tail -f /etc/hosts
-
 docker run --name 3dgs -d --gpus=all 3dgs:latest sleep infinity
 
 ####
-project=my_project
+project=my-project
 
-docker run -d --name 3dgs_$project --gpus=all -v $project:/home/d2jvkpn/aa \
-  3dgs:latest sleep infinity
-
-docker exec -it 3dgs_$project bash
-
-####
-project=my_project
-
-ls $project/images
-
-docker run -d --name 3dgs_$project --gpus=all \
-  -v $project:/home/d2jvkpn/3dgs 3dgs:latest \
-  bash /home/d2jvkpn/3dgs_pipeline.sh
+docker run -it --gpus=all \
+  --name 3dgs_$project -v $project:/home/d2jvkpn/3dgs_workspace \
+  3dgs:latest bash /home/d2jvkpn/3dgs_pipeline.sh
