89e007743ed982fbba6d5ea84a23c8c163b00981
d2jvkpn <d2jvkpn@noreply.local>
2023-10-23 09:54:58 +0800

    ...

--- services/demo-api/deployments/docker_deploy.sh
+++ services/demo-api/deployments/docker_deploy.sh
@@ -6,12 +6,18 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 export APP_Name=$(yq .app project.yaml)
 export TAG=$1 APP_ENV=$2 HTTP_PORT=$3 RPC_PORT=$4
 
+container=${APP_Name}_${APP_ENV}
+
 mkdir -p configs logs data
 envsubst < ${_path}/docker_deploy.yaml > docker-compose.yaml
 
 # docker-compose pull
-[ ! -z "$(docker ps --all --quiet --filter name=${APP_Name}_${APP_ENV})" ] &&
-  docker rm -f ${APP_Name}_$APP_ENV
-# docker-compose down for running containers only, not stopped containers
+[ ! -z "$(docker ps --all --quiet --filter name=$container)" ] &&
+  docker rm -f $container
+# 'docker-compose down' removes running containers only, not stopped containers
 
-docker-compose up -d
+UID=$UID GID=$(id -g) docker-compose up -d
+
+exit 0
+docker stop $container && docker stop $container
+docker rm -f $container
