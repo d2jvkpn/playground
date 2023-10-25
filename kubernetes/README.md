c379a090659f5492133955ca4acc1a845a1fa007
d2jvkpn <d2jvkpn@noreply.local>
2023-10-25 16:42:04 +0800

    ...

--- kubernetes/README.md
+++ kubernetes/README.md
@@ -107,7 +107,7 @@ ansible $cp_node -m shell -a "sudo bash k8s_scripts/kube_apply_flannel.sh"
 ansible $cp_node -m shell -a "sudo bash k8s_scripts/kube_apply_ingress.sh $ingress_node"
 ansible $cp_node -m shell -a "sudo bash k8s_scripts/kube_tcp-services.sh postgres 5432"
 
-ansible $cp_node -m shell -a "sudo kubeclt apply -f k8s_apps/metrics-server_components.yaml"
+ansible $cp_node -m shell -a "sudo bash k8s_scripts/kube_apply_metrics-server.sh"
 
 ansible $cp_node -m shell -a "sudo bash k8s_scripts/kube_storage_nfs.sh $cp_node 10Gi"
 
---
+++ kubernetes/k8s_scripts/kube_apply_metrics-server.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+## https://kubernetes.io/docs/tasks/run-application/horizontal-pod-autoscale-walkthrough/
+## https://www.kubecost.com/kubernetes-autoscaling/kubernetes-hpa/
+
+awk '
+  /InternalIP,ExternalIP,Hostname/{
+    sub("InternalIP,ExternalIP,Hostname", "InternalIP", $0);
+    print "        - --kubelet-insecure-tls";
+  }
+  {print}' \
+  k8s_apps/metrics-server_components.yaml > k8s_apps/data/metrics-server_components.yaml
+
+kubectl apply -f k8s_apps/data/metrics-server_components.yaml
