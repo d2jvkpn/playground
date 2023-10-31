7d0d1a2bc4e1bc68a6bc61cc9dc389440a427466
d2jvkpn <d2jvkpn@noreply.local>
2023-10-31 10:52:11 +0800

    ...

---
+++ linux/prototype/_hashmap.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+declare -A data
+
+data["cat"]="meow"
+data["dog"]="woof"
+
+echo "==> data has ${#data[@]} elements"
+
+echo ${data["cat"]}
+
+for key in ${!data[@]}; do
+    echo "==> key: $key"
+done
+
+for value in ${data[@]}; do
+    echo "==> value: $value"
+done
