a4aa0ec88a0d3c6850517dd7afecad46b6389ad9
d2jvkpn <d2jvkpn@noreply.local>
2023-11-16 16:59:50 +0800

    ...

--- kubernetes/k8s_scripts/k8s_node_remove.sh
+++ kubernetes/k8s_scripts/k8s_node_remove.sh
@@ -5,14 +5,16 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 node=$1
 
-# kubectl cordon $node
-# kubectl get pods -o wide
-# kubectl delete pod pod001
-# kubectl uncordon $node
-
 # kubectl drain $node
 kubectl drain $node --ignore-daemonsets --delete-local-data
 kubectl delete node $node
 
-# if $node is control-plane node, run this command on $node
-# kubeadm reset -f
+exit
+kubectl cordon $node
+kubectl get pods -o wide
+kubectl delete pod $pod
+kubectl uncordon $node
+
+exit
+#### control-plane node
+kubeadm reset -f
