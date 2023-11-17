472bfecb764646be13dce8113b70e5b9767b641d
d2jvkpn <d2jvkpn@noreply.local>
2023-11-17 15:38:06 +0800

    ...

--- docker/ipython/bin/entrypoint.sh
+++ docker/ipython/bin/entrypoint.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 set -eu -o pipefail
 
-# _wd=$(pwd)
-# _path=$(dirname $0 | xargs -i readlink -f {})
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
 # set -x
 
 source /app/venv/bin/activate
