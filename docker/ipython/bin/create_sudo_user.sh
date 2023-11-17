7e3e15713d9288276557710e60691eb422cf55f9
d2jvkpn <d2jvkpn@noreply.local>
2023-11-17 15:59:37 +0800

    ...

---
+++ docker/ipython/bin/create_sudo_user.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+# set -x
+
+username=$1
+
+# -u 1000
+useradd -m -s /bin/bash $username
+usermod -aG sudo $username
+
+mkdir -p /etc/sudoers.d
+echo "$username ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$username
