c06f1061262ff0cc10d03d3843309b79d3ca0aec
d2jvkpn <d2jvkpn@noreply.local>
2023-11-17 14:16:23 +0800

    ...

--- docker/ipython/docker_build.sh
+++ docker/ipython/docker_build.sh
@@ -1,22 +1,25 @@
 #! /usr/bin/env bash
 set -eu -o pipefail
+
 _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
+# set -x
 
-tag=dev; [ $# -gt 0 ] && tag=$1
-image=registry.cn-shanghai.aliyuncs.com/d2jvkpn/ipython
+image_tag=${1:-dev}
+REGION=${REGION:-}
 
-REGION=$(printenv REGION || true)
+image_name=registry.cn-shanghai.aliyuncs.com/d2jvkpn/ipython
+image=${image_name}:${image_tag}
 
 docker pull ubuntu:22.04
 
 docker build --no-cache -f ${_path}/Dockerfile \
   --build-arg=REGION="$REGION" \
-  --tag $image:$tag ./
+  --tag $image ./
 
-docker push $image:$tag
-docker images $image
+docker push $image
+# docker images $image_name
 
-for img in $(docker images --filter "dangling=true" --quiet $image); do
+for img in $(docker images --filter "dangling=true" --quiet $image_name); do
     docker rmi $img || true
 done &> /dev/null
