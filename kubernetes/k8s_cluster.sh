18386784863b8e4fe2a94d18d0446d0720593381
d2jvkpn <d2jvkpn@noreply.local>
2023-11-24 12:45:28 +0800

    ...

--- kubernetes/k8s_cluster.sh
+++ kubernetes/k8s_cluster.sh
@@ -7,6 +7,10 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 action=$1
 
+function on_exit() {
+    date +"==> %FT%T%:z exit" >> logs/k8s_up.log
+}
+
 case $action in
 "check")
     ls k8s_apps/{k8s.yaml,kube-flannel.yaml} \
@@ -21,34 +25,37 @@ case $action in
     command -v ansible
     command -v virsh
     ;;
+
 "up")
-	mkdir -p logs
-	echo "================================================================" > logs/k8s_up.log
+    mkdir -p logs
+    echo "================================================================" >> logs/k8s_up.log
+    trap on_exit EXIT
 
-	date +"==> %FT%T%:z step01_kvm_node.sh" >> logs/k8s_up.log
-	bash step01_kvm_node.sh ubuntu k8s-cp01
+    date +"==> %FT%T%:z step01_kvm_node.sh" >> logs/k8s_up.log
+    bash step01_kvm_node.sh ubuntu k8s-cp01
 
-	date +"==> %FT%T%:z step02_clone_nodes.sh" >> logs/k8s_up.log
-	bash step02_clone_nodes.sh k8s-cp01 \
-	  k8s-cp02 k8s-cp03 \
-	  k8s-node01 k8s-node02 k8s-node03 k8s-node04
+    date +"==> %FT%T%:z step02_clone_nodes.sh" >> logs/k8s_up.log
+    bash step02_clone_nodes.sh k8s-cp01 \
+      k8s-cp02 k8s-cp03 \
+      k8s-node01 k8s-node02 k8s-node03 k8s-node04
 
-	date +"==> %FT%T%:z step03_cluster_up.sh" >> logs/k8s_up.log
-	bash step03_cluster_up.sh k8s-cp01 k8s-node01
+    date +"==> %FT%T%:z step03_cluster_up.sh" >> logs/k8s_up.log
+    bash step03_cluster_up.sh k8s-cp01 k8s-node01
 
-	date +"==> %FT%T%:z step04_kube_apply.sh" >> logs/k8s_up.log
-	bash step04_kube_apply.sh k8s-cp01 k8s-node01
+    date +"==> %FT%T%:z step04_kube_apply.sh" >> logs/k8s_up.log
+    bash step04_kube_apply.sh k8s-cp01 k8s-node01
+    ;;
 
-	date +"==> %FT%T%:z done" >> logs/k8s_up.log
-	;;
 "down")
-	ansible k8s_all -m shell --become -a 'shutdown now'
-	;;
+    ansible k8s_all -m shell --become -a 'shutdown now'
+    ;;
+
 "erase")
     for node in $(awk '{print $2}' configs/kvm_k8s.txt); do
-	    bash ../kvm/src/virsh_delete.sh $node || true
-	done
-	;;
+        bash ../kvm/src/virsh_delete.sh $node || true
+    done
+    ;;
+
 *)
     >&2 echo "unknown action"
     exit 1
