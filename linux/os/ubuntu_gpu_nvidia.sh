18d35ed4866a96ccfd30ce92e19e142a4e7fbf99
d2jvkpn <d2jvkpn@noreply.local>
2023-11-27 17:46:00 +0800

    ...

---
+++ linux/os/ubuntu_gpu_nvidia.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+####
+# OS: Ubuntu 22.04
+# GPU: NVIDIA GeForce RTX 3070
+# https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-22-04
+# https://forums.developer.nvidia.com/t/nvidia-driver-is-not-working-on-ubuntu-22-04/232243
+lshw -c display
+lshw -c video
+
+sudo ubuntu-drivers devices
+sudo ubuntu-drivers autoinstall
+
+sudo reboot now
+
+####
+exit
+
+sudo apt install dkms
+sudo apt install nvidia-dkms-525
+dkms status
+
+sudo apt install nvidia-driver-525
