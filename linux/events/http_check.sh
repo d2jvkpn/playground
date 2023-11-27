ec6be907d1d4f92ff491788c689df3f6fc0fb846
d2jvkpn <d2jvkpn@noreply.local>
2023-11-27 12:56:22 +0800

    ...

---
+++ linux/events/http_check.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+url=$1
+timeout_secs=${2:-300}
+curl_args=${curl_args:-""}
+
+n=1
+# --connect-timeout
+
+echo "==> $(date +%FT%T%:z) http_check start: timeout_secs=$timeout_secs, curl_args=\"$curl_args\""
+
+while [[ $(2>&1 curl -s -I --max-time 3 $url $curl_args | awk 'NR==1{print $2; exit}') != "200" ]]; do
+    sleep 1; echo -n "."
+
+    n=$((n+1)); [ $((n%60)) -eq 0 ] && echo ""
+
+    if [[ $timeout_secs -gt 0 && $n -gt $timeout_secs ]]; then
+        >&2 echo 'http_check abort!!!'
+        exit 1
+    fi
+done
+echo ""
+
+echo "==> $(date +%FT%T%:z) http_check exit: ok"
