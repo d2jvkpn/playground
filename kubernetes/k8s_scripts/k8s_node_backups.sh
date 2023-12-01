bc912bb144fbc0c0c99dadde00ca8fb9ea6d42e1
d2jvkpn <d2jvkpn@noreply.local>
2023-12-01 14:14:01 +0800

    ...

--- kubernetes/k8s_scripts/k8s_node_backups.sh
+++ kubernetes/k8s_scripts/k8s_node_backups.sh
@@ -7,22 +7,24 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 endpoints=${1:-127.0.0.1:2379}
 
-mkdir -p k8s_apps/backups
-tag=$(date +%FT%H-%M-%S)
+out_dir=k8s_apps/data/backups_$(date +%FT%H-%M-%S)
+db=$out_dir/etcd_snapshot.db
 # sudo apt install etcd-client
 
-kubectl get cm -A -o yaml > k8s_apps/backups/configmaps.$tag.yaml
-kubectl get secrets -A -o yaml > k8s_apps/backups/secrets.$tag.yaml
+mkdir -p $out_dir
 
-db=k8s_apps/backups/etcd_snapshot.$tag.db
+kubectl get cm -A -o yaml > $out_dir/configmaps.yaml
+kubectl get secrets -A -o yaml > $out_dir/secrets.yaml
 
-sudo ETCDCTL_API=3 etcdctl --endpoints=$endpoints  \
-  --cert=/etc/kubernetes/pki/etcd/server.crt  --key=/etc/kubernetes/pki/etcd/server.key \
+sudo ETCDCTL_API=3 etcdctl --endpoints=$endpoints \
+  --cert=/etc/kubernetes/pki/etcd/server.crt \
+  --key=/etc/kubernetes/pki/etcd/server.key \
   --cacert=/etc/kubernetes/pki/etcd/ca.crt snapshot save $db
 
 sudo ETCDCTL_API=3 etcdctl --write-out=table snapshot status $db
 
 # member list
 exit
+
 sudo ETCDCTL_API=3 etcdctl --endpoints 10.2.0.9:2379 snapshot restore $db
 sudo ETCDCTL_API=3 etcdctl snapshot restore $db --data-dir /var/lib/etcd
