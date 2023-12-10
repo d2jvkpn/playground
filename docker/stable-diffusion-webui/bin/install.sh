cc62c1bb5496aefb414765bebbe310a17c8f3a03
d2jvkpn <d2jvkpn@noreply.local>
2023-12-10 10:34:34 +0800

    ...

--- docker/stable-diffusion-webui/bin/install.sh
+++ docker/stable-diffusion-webui/bin/install.sh
@@ -5,6 +5,20 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 # set -x
 
+#SD_Version=1.6.0
+
+#apt install -y --no-install-recommends \
+#  git wget tzdata pkg-config python3 python3-pip python3-venv \
+#  libgl1 libglib2.0-0 libsm6 libxrender1 libxext6
+
+#sdw=stable-diffusion-webui; \
+#  link=https://github.com/AUTOMATIC1111/${sdw}/archive/refs/tags/v${SD_Version}.tar.gz; \
+#  wget -O ${sdw}-${SD_Version}.tar.gz $link && \
+#  tar -xf ${sdw}-${SD_Version}.tar.gz && \
+#  mv ${sdw}-${SD_Version} sd-webui
+
+#cd sd-webui
+
 # git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui ./
 
 python3 -m venv venv/ && \
