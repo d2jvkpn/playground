373c80b603392b6b0db69c9772a994289f9747e2
d2jvkpn <d2jvkpn@noreply.local>
2023-12-11 15:42:20 +0800

    ...

--- swarm/kafka-kraft/bin/entrypoint.sh
+++ swarm/kafka-kraft/bin/entrypoint.sh
@@ -4,11 +4,11 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
 # PWD: /app
-cluster_id=$(awk '/^cluster_id: /{print $2; exit}' ./configs/kafka.yaml)
+cluster_id=$(awk '/^cluster_id: /{print $2; exit}' ./kafka/configs/kafka.yaml)
 
-[ -z "$cluster_id" ] && { >&2 echo "cluster_id is unset in ./configs/kafka.yaml"; exit 1; }
+[ -z "$cluster_id" ] && { >&2 echo "cluster_id is unset in ./kafka/configs/kafka.yaml"; exit 1; }
 
-kafka-storage.sh format --ignore-formatted -t $cluster_id -c ./configs/server.properties
+kafka-storage.sh format --ignore-formatted -t $cluster_id -c ./kafka/configs/server.properties
 
-kafka-server-start.sh ./configs/server.properties
-# kafka-server-start.sh -daemon ./configs/server.properties
+kafka-server-start.sh ./kafka/configs/server.properties
+# kafka-server-start.sh -daemon ./kafka/configs/server.properties
