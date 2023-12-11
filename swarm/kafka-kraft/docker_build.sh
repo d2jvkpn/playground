972def32b34b0bdbfc3464becab9ee7ba243d62e
d2jvkpn <d2jvkpn@noreply.local>
2023-12-11 17:46:13 +0800

    ...

--- swarm/kafka-kraft/docker_build.sh
+++ swarm/kafka-kraft/docker_build.sh
@@ -4,20 +4,21 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
 # set -x
+BUILD_Region=$${BUILD_Region:-}
 KAFKA_Version=${KAFKA_Version:-3.6.1}
 SCALA_Version=${SCALA_Version:-2.13}
-
-BUILD_Region=$(printenv BUILD_Region || true)
+DOCKER_Push=${DOCKER_Push:-"true"}
 
 name=registry.cn-shanghai.aliyuncs.com/d2jvkpn/kafka
 image=$name:$KAFKA_Version
 
+####
 docker pull alpine:3
 
 docker build --no-cache \
-  --build-arg=SCALA_Version="$SCALA_Version" \
-  --build-arg=KAFKA_Version="$KAFKA_Version" \
   --build-arg=BUILD_Region="$BUILD_Region" \
+  --build-arg=KAFKA_Version="$KAFKA_Version" \
+  --build-arg=SCALA_Version="$SCALA_Version" \
   -f ${_path}/Dockerfile -t $image ./
 
 docker images -q -f dangling=true $name | xargs -i docker rmi {}
@@ -27,6 +28,5 @@ docker images -q -f dangling=true $name | xargs -i docker rmi {}
 # [ "$ans" != "yes" ] && exit 0
 
 # DOCKER_Push=$(printenv DOCKER_Push || true)
-DOCKER_Push=${DOCKER_Push:-"true"}
 [ "$DOCKER_Push" == "false" ] && exit 0
 docker push $image
