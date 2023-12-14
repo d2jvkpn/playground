dc8f3ff290fffcd416085ac76eee8b08f253f8a3
d2jvkpn <d2jvkpn@noreply.local>
2023-12-14 10:04:38 +0800

    ...

--- kubernetes/step05_post.sh
+++ kubernetes/step05_post.sh
@@ -9,3 +9,14 @@ cp_node=$1
 
 # rsync -arPv $cp_node:k8s_apps/data/ k8s_apps/data/
 ansible $cp_node -m synchronize -a "mode=pull src=k8s_apps/data/ dest=./k8s_apps/data"
+
+exit
+# ansible k8s_all -m shell --become -a 'apt update'
+# ansible k8s_all -m shell --become -a 'DEBIAN_FRONTEND=nointeractive apt -y upgrade'
+
+ansible k8s_all -m apt --become -a 'update_cache=yes'
+ansible k8s_all -m apt --become -a 'upgrade=yes'
+
+ansible k8s_all -m apt --become -a 'clean=yes autoclean=yes autoremove=yes'
+
+ansible-playbook -v reboot-required.yaml --inventory=configs/k8s_hosts.ini --extra-vars="host=k8s_all"
