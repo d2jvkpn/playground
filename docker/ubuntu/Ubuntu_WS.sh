375fccb7dcc2b359f5b1ea5f7ae8ced70db6d28e
d2jvkpn <d2jvkpn@noreply.local>
2023-12-18 13:08:19 +0800

    ...

--- docker/ubuntu/Ubuntu_WS.sh
+++ docker/ubuntu/Ubuntu_WS.sh
@@ -5,15 +5,21 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 # set -x
 
-container_name=ubuntu-ws_$(tr -dc 'a-zA-Z0-9' < /dev/random | fold -w 16 | head -n1 || true)
+container_name=ubuntu-ws_$(tr -dc 'a-z0-9' < /dev/random | fold -w 16 | head -n1 || true)
 
 mount_path=${mount_path:-""}
+uid=$(id -u)
+gid=$(id -g)
+
+echo "==> mount_path: $mount_path"
+
+# set -x
 
 if [ -z "$mount_path" ]; then
-    docker run -d --name $container_name -w /app ubuntu:22.04 tail -f /etc/hosts
+    docker run -d --name $container_name -u $uid:$gid  -w /app ubuntu:22.04 tail -f /etc/hosts
 else
     mount_path=$(readlink -f $mount_path)
-    docker run -d --name $container_name -w /app $mount_path:/app/data ubuntu:22.04 sleep infinity
+    docker run -d --name $container_name -u $uid:$gid -w /app -v $mount_path:/app ubuntu:22.04 sleep infinity
 fi
 
 echo "docker exec -it $container_name bash"
