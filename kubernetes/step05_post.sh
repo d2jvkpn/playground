995ee3d64f09316b0ac3fd3098a28cc19e01298b
d2jvkpn <d2jvkpn@noreply.local>
2023-12-18 13:54:29 +0800

    ...

--- kubernetes/step05_post.sh
+++ kubernetes/step05_post.sh
@@ -20,6 +20,7 @@ ansible $cp_node -a "bash k8s_scripts/kube_tcp-services.sh postgres 5432"
 ansible $cp_node -m synchronize -a "mode=pull src=k8s_apps/data/ dest=./k8s_apps/data"
 
 exit
+####
 # ansible k8s_all -m shell --become -a 'apt update'
 # ansible k8s_all -m shell --become -a 'DEBIAN_FRONTEND=nointeractive apt -y upgrade'
 
@@ -30,9 +31,10 @@ ansible k8s_all -m apt --become -a 'upgrade=yes'
 
 ansible k8s_all -m apt --become -a 'clean=yes autoclean=yes autoremove=yes'
 
-# --inventory=configs/k8s_hosts.ini
-ansible-playbook reboot-required.yaml -v --extra-vars="host=k8s_all"
-
+####
 ansible k8s_all -a 'uptime'
 
 ansible k8s_cps[0] -m shell -a "kubectl get pods -A -o wide | sort -k 8"
+
+# --inventory=configs/k8s_hosts.ini
+ansible-playbook reboot-required.yaml -v --extra-vars="host=k8s_all"
