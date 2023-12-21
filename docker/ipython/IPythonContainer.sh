03abd496067f6a641e13f43b0bcd5e66e81e0a45
d2jvkpn <d2jvkpn@noreply.local>
2023-12-21 11:26:08 +0800

    ...

--- docker/ipython/IPythonContainer.sh
+++ docker/ipython/IPythonContainer.sh
@@ -5,11 +5,12 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 # set -x
 
-UserID=$(id -u)
-GroupID=$(id -g)
+UserID=$(id -u); GroupID=$(id -g)
+mount_path=${mount_path:-data}
+mount_path=$(readline -f $mount_path)
 
-mkdir -p data
+mkdir -p $mount_path
 
-docker run -d --name ipython -v $PWD/data:/app/data \
+docker run -d --name ipython -v $mount_path:/app/data \
   --user ${UserID}:${GroupID} -v GroupID=${GroupID} \
   registry.cn-shanghai.aliyuncs.com/d2jvkpn/ipython:dev sleep infinity
