981e7b2f0e6834ce707fc4633f7a072c4540e6b6
d2jvkpn <d2jvkpn@noreply.local>
2023-12-25 20:51:21 +0800

    ...

---
+++ linux/os/rockylinux_config.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+# set -x
+
+yum update
+
+dnf install epel-release
+
+yum install jq
--- linux/os/ubuntu_config.sh
+++ linux/os/ubuntu_config.sh
@@ -55,6 +55,29 @@ cat >> ~/.profile <<'EOF'
 echo "==> Welcome $(date +%FT%T.%N:%:z)"
 EOF
 
+exit
+#### disable ads
+sudo systemctl disable ubuntu-advantage
+sudo pro config set apt_news=false
+
+####
+sudo cp /usr/lib/update-notifier/apt_check.py /usr/lib/update-notifier/apt_check.py.bk
+
+sudo sed -Ezi.orig \
+  -e 's/(def _output_esm_service_status.outstream, have_esm_service, service_type.:\n)/\1    return\n/' \
+  -e 's/(def _output_esm_package_alert.*?\n.*?\n.:\n)/\1    return\n/' \
+  /usr/lib/update-notifier/apt_check.py
+
+sudo /usr/lib/update-notifier/update-motd-updates-available --force
+
+sudo sed -i '/^deb/s/^/#-- /' /var/lib/ubuntu-advantage/apt-esm/etc/apt/sources.list.d/ubuntu-esm-apps.list
+
+sudo sed -i '/ENABLED/s/1/0/' /etc/default/motd-news
+#- sudo sed -i '/=motd.dynamic/s/^/#-- /' /etc/pam.d/sshd
+
+# sudo apt install landscape-common
+# landscape-sysinfo
+
 #### install
 exit
 
