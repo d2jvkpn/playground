68b259753350e05cf6aa6817ff31f746f9637ae5
d2jvkpn <d2jvkpn@noreply.local>
2024-01-04 17:30:41 +0800

    ...

---
+++ linux/nmap/nmap_a01.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+# set -x
+
+# sudo apt install -y nmap
+
+dev=$(ip -o -4 route show to default | awk '{print $5}')
+host_ip=$(ip -o -4 route show | awk -v dev=$dev '$3==dev{print $1}')
+
+# 192.168.1.42/24
+# nmap -sn $host_ip |
+#   awk '/Nmap scan report for/{print $NF}' |
+#   sed 's/^(//; s/)$//'
+
+nmap --open -p 80,445 $host_ip
