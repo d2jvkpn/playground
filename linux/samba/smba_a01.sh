8733447be5d5dae8e499d512e6d3d48b2ad13412
d2jvkpn <d2jvkpn@noreply.local>
2024-01-08 10:24:26 +0800

    ...

--- linux/samba/smba_a01.sh
+++ linux/samba/smba_a01.sh
@@ -6,28 +6,33 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 smb_addr=//192.168.1.42/project
 username=bob
 password=123456
-mount_point=/mnt/samba/project
-cred_file=~/.config/samba/$username.conf
 
-echo "==> samba address: smb:$smb_addr, mount_point: $mount_point"
-sudo mkdir -p $mount_point ~/.config/samba
+####
+mount_point=/mnt/samba/$(basename $smb_addr)
+cred_file=~/.config/samba/${username}-$(echo $smb_addr | awk -F "/" '{print $3}').conf
 
-sudo chown -R `whoami` $mount_point ~/.config/samba
+echo "==> smb_addr: $smb_addr, mount_point: $mount_point, cred_file: $cred_file"
+
+sudo mkdir -p $mount_point
+sudo chown -R `whoami` $mount_point
 sudo chmod 700 $mount_point
 
-[ -s $cred_file ] || cat > $cred_file << EOF
+if [ ! -s $cred_file ]; then
+    mkdir -p $(dirname $cred_file)
+cat > $cred_file << EOF
 username=$username
 password=$password
 EOF
+# chmod 600 $cred_file
+fi
 
-chmod 600 $cred_file
-
+####
 sudo mount -t cifs \
   -o ro,file_mode=0777,dir_mode=0777,vers=3.0 \
   -o iocharset=utf8,credentials=$cred_file \
   $smb_addr $mount_point
 
-ls $mount_point
+ls -alt $mount_point
 
 exit
 sudo umount $mount_point
