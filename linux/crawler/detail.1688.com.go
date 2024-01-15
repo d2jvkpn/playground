6c8e851fc4288d39bc6fee164597b0bfe89e0958
d2jvkpn <d2jvkpn@noreply.local>
2024-01-15 15:52:20 +0800

    ...

--- linux/crawler/detail.1688.com.go
+++ linux/crawler/detail.1688.com.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -21,6 +22,67 @@ var (
 	// _Logger       = slog.New(slog.NewTextHandler(os.Stderr, nil))
 	_Logger = slog.New(slog.NewJSONHandler(os.Stderr, nil))
 )
+
+func main() {
+	var (
+		addr        string
+		certFile    string
+		keyFile     string
+		origin      string
+		enableHttps bool
+		err         error
+		listener    net.Listener
+		cert        tls.Certificate
+		mux         *http.ServeMux
+		server      *http.Server
+	)
+
+	flag.StringVar(&addr, "addr", ":3000", "http service address")
+	flag.StringVar(&certFile, "cert", "", "tls cert file")
+	flag.StringVar(&keyFile, "key", "", "tls key file")
+	flag.StringVar(&origin, "origin", "*", "cros allow origin")
+	flag.Parse()
+
+	/*
+		http.HandleFunc("/", archive)
+
+		log.Printf("==> Http service is listening on %q\n", addr)
+		if err = http.ListenAndServe(addr, nil); err != nil {
+			log.Fatalln(err)
+		}
+	*/
+
+	if listener, err = net.Listen("tcp", addr); err != nil {
+		log.Fatalln(err)
+	}
+
+	mux, server = http.NewServeMux(), new(http.Server)
+	server.Handler = mux
+
+	enableHttps = certFile != "" && keyFile != ""
+	if enableHttps {
+		if cert, err = tls.LoadX509KeyPair(certFile, keyFile); err != nil {
+			log.Fatalln(err)
+		}
+		server.TLSConfig = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+	}
+
+	// TODO: configure server, e.g. BaseContext
+	// mux.Handle("/", middlewareHandler(http.HandlerFunc(archive)))
+	mux.HandleFunc("/", middlewareFunc("PUT", origin, archive))
+
+	if enableHttps {
+		log.Printf("==> Https service is listening on %q\n", addr)
+	} else {
+		log.Printf("==> Http service is listening on %q\n", addr)
+	}
+
+	if err = server.Serve(listener); err != nil {
+		log.Fatalln(err)
+	}
+}
 
 func middlewareHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -43,9 +105,9 @@ func middlewareHandler(next http.Handler) http.Handler {
 	})
 }
 
-func middlewareFunc(method string, fn http.HandlerFunc) http.HandlerFunc {
+func middlewareFunc(method, origin string, fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		// Content-Type, Authorization, X-CSRF-Token
 		w.Header().Set(
@@ -71,23 +133,18 @@ func middlewareFunc(method string, fn http.HandlerFunc) http.HandlerFunc {
 		fn(w, r)
 
 		data := getData(r)
-		if err, ok := data["error"].(error); ok && err == nil {
-			_Logger.Info(
-				fmt.Sprintf("%s@%s", r.Method, r.URL.Path),
-				"query", r.URL.RawQuery,
-				"ip", r.RemoteAddr,
-				"elapsed", time.Since(now).String(),
-				"data", data,
-			)
-		} else {
-			_Logger.Info(
-				fmt.Sprintf("%s@%s", r.Method, r.URL.Path),
-				"query", r.URL.RawQuery,
-				"ip", r.RemoteAddr,
-				"elapsed", time.Since(now).String(),
-				"data", data,
-			)
+		logger := _Logger.Info
+		if err, ok := data["error"].(error); ok && err != nil {
+			logger = _Logger.Error
 		}
+
+		logger(
+			fmt.Sprintf("%s@%s", r.Method, r.URL.Path),
+			"query", r.URL.RawQuery,
+			"ip", r.RemoteAddr,
+			"elapsed", time.Since(now).String(),
+			"data", data,
+		)
 	}
 }
 
@@ -120,10 +177,13 @@ func archive(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
 		// json.NewEncoder(w)
 		bts, _ := json.Marshal(map[string]any{"code": code, "msg": msg})
-		setData(r, map[string]any{
-			"status": status, "error": err,
-			"code": code, "msg": msg,
-		})
+
+		if err == nil {
+			setData(r, map[string]any{"status": status, "code": code, "msg": msg})
+		} else {
+			setData(r, map[string]any{"status": status, "error": err, "code": code, "msg": msg})
+		}
+
 		w.Write(bts)
 	}
 
@@ -161,42 +221,5 @@ func archive(w http.ResponseWriter, r *http.Request) {
 	} else {
 		code, msg = 0, "ok"
 		response(http.StatusOK)
-	}
-}
-
-func main() {
-	var (
-		addr     string
-		err      error
-		listener net.Listener
-		mux      *http.ServeMux
-		server   *http.Server
-	)
-
-	flag.StringVar(&addr, "addr", ":3000", "http service address")
-	flag.Parse()
-
-	/*
-		http.HandleFunc("/", archive)
-
-		log.Printf("==> Http service is listening on %q\n", addr)
-		if err = http.ListenAndServe(addr, nil); err != nil {
-			log.Fatalln(err)
-		}
-	*/
-
-	if listener, err = net.Listen("tcp", addr); err != nil {
-		log.Fatalln(err)
-	}
-
-	mux, server = http.NewServeMux(), new(http.Server)
-	server.Handler = mux
-	// TODO: configure server, e.g. BaseContext
-	// mux.Handle("/", middlewareHandler(http.HandlerFunc(archive)))
-	mux.HandleFunc("/", middlewareFunc("PUT", archive))
-
-	log.Printf("==> Http service is listening on %q\n", addr)
-	if err = server.Serve(listener); err != nil {
-		log.Fatalln(err)
 	}
 }
