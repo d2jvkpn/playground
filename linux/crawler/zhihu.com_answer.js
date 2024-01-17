08bbfb2c7a26313e0c5af126c3c334826794508f
d2jvkpn <d2jvkpn@noreply.local>
2024-01-18 01:27:15 +0800

    ...

--- linux/crawler/zhihu.com_answer.js
+++ linux/crawler/zhihu.com_answer.js
@@ -1,14 +1,16 @@
 var target = document.querySelectorAll(".AnswerCard")[0];
+
 if (!target) {
     target = document.querySelectorAll(".RichContent")[0];
 }
 
 // var target = document.querySelectorAll(".MoreAnswers")[0];
 
-var data = `${document.URL}\n\n${target.innerText}`;
+var text = target.innerText.replace(/\n+/g, "\n");
+var data = `${document.URL}\n\n${text}`;
 
 var link = document.createElement("a");
 link.href = `data:text/plain;charset=utf8,${data.replace(/\n/g, "%0D%0A")}\n`;
 
-link.download = `${document.title}.md`;
+link.download = `zhihu.com_${document.title}.md`;
 link.click();
