40431e7ece14923d92de9b04cf4b8bb78c2d154f
d2jvkpn <d2jvkpn@noreply.local>
2024-01-19 09:45:01 +0800

    ...

--- linux/crawler/wget_download.sh
+++ linux/crawler/wget_download.sh
@@ -6,11 +6,34 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 # jf=$1
 key=${key:-links}
 
+function download() {
+    name=$1
+    link=$2
+
+    [ -f $name ] && continue
+    wget -O "$name.temp" $link
+    mv "$name.temp" "$name"
+}
+
+export -f download
+
 for jf in "$@"; do
     n=0
     for link in $(jq -r ".$key[]" $jf); do
         n=$((n+1))
         name=$(basename $link)
         name=$(dirname $jf)/$(printf "%03d_%s" $n $name)
+        # wget -cO $name $link
      done
-done | xargs -n 2 -P 8 wget -cO
+done | xargs -n 2 -P 8 bash -c 'download "$@"' _
+
+exit
+for jf in "$@"; do
+    n=0
+    for link in $(jq -r ".$key[]" $jf); do
+        n=$((n+1))
+        name=$(basename $link)
+        name=$(dirname $jf)/$(printf "%03d_%s" $n $name)
+        # wget -cO $name $link
+     done
+done | xargs -n 2 -P 8 wget -O
