6778cc168cf0f1c3d54abf7d45ef2b0493cd9fb7
d2jvkpn <d2jvkpn@noreply.local>
2024-02-19 12:10:04 +0800

    ...

---
+++ linux/shell/du-sort-by-size.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+# set -x
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+
+du -sh -- *  | sort -rh
+
+exit
+
+du -sk -- * | sort -nr | awk -F '\t' '{print "\""$2"\""}' | xargs du -sh
