bffa7d9f2264bede0fad5b92a028dff0445d4788
d2jvkpn <d2jvkpn@noreply.local>
2024-02-21 09:12:29 +0800

    ...

--- linux/shell/CreateDateDir.sh
+++ linux/shell/CreateDateDir.sh
@@ -5,18 +5,17 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
 
-prefix=${1:-""}
+outdir=${1:-""}
 clock=${clock:-0}
 
 if [ "${clock}" == "1" ]; then
-    outdir=$(date +%FT%H-%M-%S)
+    outdir=${outdir}$(date +%FT%H-%M-%S)
 elif [ "${clock}" == "2" ]; then
-    outdir=$(date +%FT%H-%M-%S-%s)
+    outdir=${outdir}$(date +%FT%H-%M-%S-%s)
 else
-    outdir=$(date +%F)
+    outdir=${outdir}$(date +%F)
 fi
 
-outdir=${prefix}${outdir}
 mkdir -p $outdir
 
->&2 echo "~~~ created directory: $outdir"
+>&2 echo "~~~ created dir: $outdir"
