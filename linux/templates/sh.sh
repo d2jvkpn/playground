e3c4879dab31594cae3ec2c6f05d7be8d592fc67
d2jvkpn <d2jvkpn@noreply.local>
2024-03-04 10:36:40 +0800

    ...

--- linux/templates/sh.sh
+++ linux/templates/sh.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
-set -eu -o pipefail
-# # set -x
-_wd=$(pwd)
-_path=$(dirname $0 | xargs -i readlink -f {})
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
 # echo "Hello, world!"
