7cc732a699234c941dea39ff20d3ab1c79421b6a
d2jvkpn <d2jvkpn@noreply.local>
2024-03-12 11:32:46 +0800

    ...

--- linux/os/torch_cuda.sh
+++ linux/os/torch_cuda.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/env bash
-set -eu -o pipefail
-# set -x
-_wd=$(pwd)
-_path=$(dirname $0 | xargs -i readlink -f {})
+set -eu -o pipefail # set -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
 ####
 sudo apt install nvidia-driver-535
@@ -17,6 +15,6 @@ pip3 show torch
 # pip freeze
 pip list | grep cuda
 
-python3 -c 'from importlib.metadata import version; print(version("torch"))'
+python3 -c 'from importlib.metadata import version; print("torch=="+version("torch"))'
 
 python3 -c 'import torch; print(torch.cuda.is_available()); print(torch.cuda.get_device_name(0))'
