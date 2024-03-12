40d4b1a5e6ca4c0ea71fa5533deb85ff98a4f1c4
d2jvkpn <d2jvkpn@noreply.local>
2024-03-12 11:28:15 +0800

    ...

---
+++ linux/os/torch_cuda.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+# set -x
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+####
+sudo apt install nvidia-driver-535
+# reboot may be required
+nvidia-smi
+
+####
+# pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install torch==2.2.1 torchvision torchaudio -i https://pypi.tuna.tsinghua.edu.cn/simple 
+
+pip3 show torch
+# pip freeze
+pip list | grep cuda
+
+python3 -c 'from importlib.metadata import version; print(version("torch"))'
+
+python3 -c 'import torch; print(torch.cuda.is_available()); print(torch.cuda.get_device_name(0))'
