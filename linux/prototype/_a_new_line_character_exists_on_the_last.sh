304d459e21c00230b8fcd7d91b58e07969bc11cf
d2jvkpn <d2jvkpn@noreply.local>
2024-03-13 16:12:14 +0800

    ...

---
+++ linux/prototype/_a_new_line_character_exists_on_the_last.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+# echo "Hello, world!"
+
+exit
+
+####
+mkdir -p data
+
+echo -en 'hello\nHello\nHELLO' > data/a.txt
+cat -A data/a.txt
+[ $(tail -n 1 data/a.txt | wc -l) -eq 0 ] && >&2 echo "No new line found at the end."
+
+####
+sed -i -e $'$a\\' data/a.txt
+[ $(tail -n 1 data/a.txt | wc -l) -gt 0 ]; && >&2 echo "New line found at the end."
