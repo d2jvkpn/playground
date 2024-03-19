6f93a97908270ade22e775871412ea5f14f29bce
d2jvkpn <d2jvkpn@noreply.local>
2024-03-19 14:32:53 +0800

    ...

--- linux/crawler/zhihu.com_answers.js
+++ linux/crawler/zhihu.com_answers.js
@@ -10,22 +10,41 @@ function getTarget() {
 
 function getText(target, archive) {
   let filename = `${document.title.split(" - ")[0]}` + 
-    `__${document.URL.split("//").pop().replace(/\//g, "-")}.md`;;
+    `__${document.URL.split("//").pop().replace(/\//g, "-")}.md`;
 
+  /*
   let data = `# ${document.title.split(" - ")[0]}\n\n` +
     `**link**: *${document.URL}*\n` +
     `**filename**: *${filename}*\n` +
     target.innerText.replace(/\n+/g, "\n");
+  */
+
+  let text = `# ${document.title.split(" - ")[0]}\n\n` +
+    `**link**: *${document.URL}*\n` +
+    `**filename**: *${filename}*\n` +
+    target.querySelector(".RichContent-inner").innerText + "\n\n" +
+    target.querySelector(".ContentItem-time").innerText + "\n\n" +
+    target.querySelector(".ContentItem-actions").children[0].innerText + "\n";
+
+  let comments = document.querySelector(".Comments-container") ||
+    document.querySelector(".Modal-content");
+
+  if (comments) {
+    let items = Array.from(comments.querySelectorAll("div")).filter(e => e.hasAttribute("data-id"));
+    items.forEach(e => {
+      text += "\n\n#### " + e.innerText;
+    });
+  }
 
   let moreAns = document.querySelector(".MoreAnswers");
   if (moreAns) {
     let links = Array.from(moreAns.querySelectorAll(".AnswerItem"))
       .map(e => document.URL.replace(/\d+$/, e.getAttribute("name")));
 
-    data += `\n#### More Answers:\n- ${links.join("\n- ")}\n`;
+    text += `\n\n#### More Answers:\n- ${links.join("\n- ")}\n`;
   }
 
-  archive(data);
+  archive(text);
 }
 
 function printData(data) {
@@ -34,7 +53,7 @@ function printData(data) {
 
 function downloadAnchor(data) {
   let link = document.createElement("a");
-  link.href = `data:text/plain;charset=utf8,${data.replace(/\n/g, "%0D%0A")}\n`;
+  link.href = `data:text/plain;charset=utf8,${data.replace(/\n/g, "%0D%0A").replace(/#/g, "%23")}\n`;
 
   link.download = `${document.title.split(" - ")[0]}` +
     `__${document.URL.split("//").pop().replace(/\//g, "-")}.md`;
@@ -70,15 +89,15 @@ function postArchive(url, data) {
 
 //
 var target = getTarget();
-var btn = Array.from(target.querySelectorAll("button")).find(e => e.innerText.includes("评论"));
+var btn = Array.from(target.querySelectorAll("button")).find(e => e.innerText.includes("条评论"));
 
 if (btn) {
   btn.click();
-  setTimeout(() => getText(target, printData), 1000);
-  // setTimeout(() => getText(target, downloadAnchor), 1000);
+  setTimeout(() => getText(target, downloadAnchor), 1000);
+  // setTimeout(() => getText(target, printData), 1000);
   // setTimeout(() => getText(target, data => postArchive(`http://127.0.0.1:3000/`, data)), 1000);
 } else {
-  getText(target, printData);
-  // getText(target, downloadAnchor);
+  getText(target, downloadAnchor);
+  // getText(target, printData);
   // getText(target, getText(target, data => postArchive(`http://127.0.0.1:3000/`, data)));
 };
