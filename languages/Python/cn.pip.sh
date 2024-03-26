6a31979d6f298b7dc3f0f7317b38b0dec4fe4e99
d2jvkpn <d2jvkpn@noreply.local>
2024-03-26 16:31:14 +0800

    ...

--- languages/Python/cn.pip.sh
+++ languages/Python/cn.pip.sh
@@ -1,7 +1,24 @@
 #! /usr/bin/env bash
-set -eu -o pipefail
-_wd=$(pwd)
-_path=$(dirname $0 | xargs -i readlink -f {})
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+pip3 config set global.index-url 'https://pypi.tuna.tsinghua.edu.cn/simple'
+pip3 config set install.trusted-host 'pypi.tuna.tsinghua.edu.cn'
+
+python3 -m pip install --upgrade pip
+
+# /etc/pip.conf
+cat > ~/.config/pip/pip.conf <<EOF
+[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+target = ~/.local/lib/python3.10/site-packages/
+
+[install]
+trusted-host = pypi.tuna.tsinghua.edu.cn
+EOF
+
+
+exit
 
 pip3 config set global.index-url 'https://pypi.douban.com/simple/'
 pip3 config set install.trusted-host 'pypi.douban/simple'
