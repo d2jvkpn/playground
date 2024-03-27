a66b827a106b6ba2bf1e67756b2275df67086c9e
d2jvkpn <d2jvkpn@noreply.local>
2024-03-27 19:29:06 +0800

    ...

--- docker/cloud-native/docker_deploy.sh
+++ docker/cloud-native/docker_deploy.sh
@@ -1,14 +1,11 @@
-#! /usr/bin/env bash
-set -eu -o pipefail
-# set -x
-_wd=$(pwd)
-_path=$(dirname $0 | xargs -i readlink -f {})
+#!/bin/bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-mkdir -p configs data/prometheus data/grafana
+mkdir -p configs logs data/prometheus data/grafana
 
 cp examples/{jaeger.yaml,otel-collector.yaml,prometheus.yaml,prometheus_web.yaml} configs/
 cp docker_deploy.yaml docker-compose.yaml
 
-mkdir -p logs
 # docker network create grafana
 docker-compose up -d
