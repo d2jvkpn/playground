e767dd0e68d01d1c9c6c6ab078360687a402b1ee
d2jvkpn <d2jvkpn@noreply.local>
2024-03-29 23:15:01 +0800

    ...

---
+++ docker/filebrowser/docker_deploy.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+mkdir -p configs logs data
+export Port=3020 UserId=$(id -u) GroupId=$(id -g)
+
+[ -s configs/filebrowser.json ] || \
+cat > configs/filebrowser.json <<EOF
+{
+  "port": $Port,
+  "baseURL": "",
+  "address": "",
+  "log": "/app/logs/filebrowser.log",
+  "database": "/app/data/filebrowser.db",
+  "root": "/srv"
+}
+EOF
+
+envsubst < docker_deploy.yaml > docker-compose.yaml
+
+docker-compose pull
+docker-compose up -d
+
+
+exit
+
+cat >> .filebrowser.json <<EOF
+{
+  "port": 80,
+  "baseURL": "",
+  "address": "",
+  "log": "stdout",
+  "database": "/database.db",
+  "root": "/srv"
+}
+EOF
+
+docker run --rm -it --entrypoint=sh filebrowser/filebrowser:v2
+
+docker run \
+    -v /path/to/root:/srv \
+    -v /path/filebrowser.db:/database.db \
+    -v /path/.filebrowser.json:/.filebrowser.json \
+    -u $(id -u):$(id -g) \
+    -p 8080:80 \
+    filebrowser/filebrowser:v2
