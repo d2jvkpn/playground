53973622f0e6ccaa67fc0cffce54ffd05b19cd8f
d2jvkpn <d2jvkpn@noreply.local>
2024-04-02 14:06:57 +0800

    ...

--- docker/nextcloud/docker_deploy.sh
+++ docker/nextcloud/docker_deploy.sh
@@ -2,9 +2,10 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-HTTP_Port=${1:-3031}
+HTTP_Port=${1:-3031}; dry_run=${dry_run:-false}
 
 mkdir -p data/nextcloud data/postgres configs
+
 password=$(tr -dc "0-9a-zA-Z" < /dev/urandom | fold -w 32 | head -n 1 || true)
 
 [ -s configs/nextcloud.env ] || \
@@ -15,6 +16,12 @@ EOF
 export HTTP_Port=$HTTP_Port USER_UID=$(id -u) USER_GID=$(id -g)
 envsubst < docker_deploy.postgres.yaml > docker-compose.yaml
 
+[ "$dry_run" == "true" ] && exit 0
+
 docker-compose up -d
 sleep 5
 docker-compose logs
+
+exit
+
+sed -i 's/  env_file/  # env_file/' docker-compose.yaml
--- docker/nextcloud/nextcloud_api.sh
+++ docker/nextcloud/nextcloud_api.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
 curl -k -u "USERNAME:P4s5word" -T "path-to-source_file" \
   "https://your.cloud.tld/remote.php/webdav/path-on-your-server/source_file"
