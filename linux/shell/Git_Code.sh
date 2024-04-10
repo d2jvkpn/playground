0bbd4f3b4f310cd91616ceb6de6ea539fba5738f
d2jvkpn <d2jvkpn@noreply.local>
2024-04-10 13:28:39 +0800

    ...

--- linux/shell/Git_Code.sh
+++ linux/shell/Git_Code.sh
@@ -1,17 +1,18 @@
-#! /usr/bin/env bash
-set -eu -o pipefail # -x
+#!/bin/bash
+set -eu -o pipefail -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
 if [ $# -gt 0 ]; then
     git clone $1 code
+    zip -qr code.zip code
     exit 0
 fi
 
-if [ ! -d code && ! -s code.zip ]; then
+if [[ ! -d code && ! -s code.zip ]]; then
     >&2 echo "==> Not code(.zip) found!"
     exit 1
-elif [ ! -d code && -s code.zip ]; then
-    unzip code.zip
+elif [[ ! -d code && -s code.zip ]]; then
+    unzip -q code.zip
 fi
 
 cd code
@@ -26,5 +27,4 @@ cd ${_path}
 
 echo "==> Replacing code.zip"
 [ -f code.zip ] && rm code.zip
-
-zip -r code.zip code
+zip -qr code.zip code
