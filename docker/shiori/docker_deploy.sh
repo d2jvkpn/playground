d99e8b34535ccb4a8b2cd423c3af73b0945d0cc1
d2jvkpn <d2jvkpn@noreply.local>
2024-04-11 17:32:22 +0800

    ...

--- docker/shiori/docker_deploy.sh
+++ docker/shiori/docker_deploy.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-export PORT=$1
+export HTTP_Port=$1
 envsubst < ${_path}/docker_deploy.yaml > docker-compose.yaml
 
 docker-compose pull
