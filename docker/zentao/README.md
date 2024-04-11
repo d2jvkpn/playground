807fc6159badb70cb87c81d892cef49c6aae9915
d2jvkpn <d2jvkpn@noreply.local>
2024-04-11 13:08:16 +0800

    ...

---
+++ docker/zentao/README.md
@@ -0,0 +1,11 @@
+### Zentao Deployment
+---
+
+#### 1. 
+```bash
+docker pull hub.zentao.net/app/zentao:latest
+```
+
+#### 2. 
+
+#### 3. 
--- linux/shell/SortBySize.sh
+++ linux/shell/SortBySize.sh
@@ -2,9 +2,13 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
+asc=${asc:-"false"}
 
-du -sh -- *  | sort -rh
+if [[ "$asc" == "true" ]]; then
+    du -sh -- *  | sort -h
+else
+    du -sh -- *  | sort -rh
+fi
 
 exit
-
 du -sk -- * | sort -nr | awk -F '\t' '{print "\""$2"\""}' | xargs du -sh
