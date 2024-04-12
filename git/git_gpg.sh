51586b6c897eafbd05a70068e1fb750890cd172f
d2jvkpn <d2jvkpn@noreply.local>
2024-04-12 11:26:42 +0800

    ...

--- git/git_gpg.sh
+++ git/git_gpg.sh
@@ -2,37 +2,42 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
+key_name=${1:-"git-johndoe"}
+password=$(tr -dc "0-9a-zA-Z" < /dev/urandom | fold -w 32 | head -n 1 || true)
+
+gpg --list-keys --keyid-format=long
 gpg --list-secret-keys --keyid-format=long
+
+ls ~/.gnupg/pubring.kbx ~/.gnupg/trustdb.gpg
+mkdir -p ~/.gnupg/private-keys-v1.d ./data
 
 # gpg --full-generate-key
 # ~/.gnupg/openpgp-revocs.d
-# ~/.gnupg/
-# gpg --armor --export KEY_Id
 
-mkdir -p data
-
-cat > data/gpg.conf <<EOF
+cat > data/${key_name}.conf <<EOF
 %echo ==> Generating a basic OpenPGP key
 Key-Type: RSA
 Key-Length: 3072
 Subkey-Type: RSA
 Subkey-Length: 3072
 Name-Real: John Doe
-Name-Comment: My comment
+Name-Comment: This is a test.
 Name-Email: johndoe@noreply.local
 Expire-Date: 0
-Passphrase: secret
-%pubring data/git-johndoe.gpg
+Passphrase: $password
+%pubring data/${key_name}.gpg
 %commit
 %echo ==> Done
 EOF
 
-gpg --batch --gen-key data/gpg.conf
-ls ~/.gnupg/ data/git-johndoe.gpg
+gpg --batch --gen-key data/${key_name}.conf
+ls ~/.gnupg/private-keys-v1.d data/${key_name}.gpg
 
-gpg --import data/git-johndoe.gpg
-
+gpg --list-secret-keys --keyid-format=long
+gpg --import data/${key_name}.gpg
 gpg --list-secret-keys --keyid-format=long
 
 exit
-gpg --armor --export KEY_Id
+
+gpg --export --armor --output data/${key_name}.key $key_id
+gpg --export-secret-keys --armor --output data/${key_name}.key $key_id
