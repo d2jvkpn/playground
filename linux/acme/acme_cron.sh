0e941362ec1658bcab6df72224f1059ea7af907c
d2jvkpn <d2jvkpn@noreply.local>
2024-04-12 16:39:37 +0800

    ...

--- linux/acme/acme_cron.sh
+++ linux/acme/acme_cron.sh
@@ -2,6 +2,7 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {} )
 
+# minute hour day_of_month month day_of_week command
 # cron: 0 0 * * * bash ${HOME}/crons/acme_cron.sh
 
 acme=~/Apps/acme # directory
@@ -34,33 +35,3 @@ exit
 kubectl -n dev create secret tls $domain --dry-run=client \
   --key ${certs_dir}/$domain.key --cert ${certs_dir}/$domain.cer -o yaml |
   kubectl apply -f -
-
-exit
-
-#### 1. installation
-mkdir -p ~/Apps ~/crons
-git clone https://github.com/acmesh-official/acme.sh ~/Apps/acme
-# curl https://get.acme.sh | sh
-
-~/Apps/acme/acme.sh --register-account -m $email
-
-#### 2. setup account
-
-##### 2.1 dns aliyun
-# account permissions: AliyunDNSFullAccess
-email=john@doe.local
-domain=doe.local
-
-# get access key https://ram.console.aliyun.com/manage/ak
-export Ali_Key="xxxx" Ali_Secret="yyyy"
-
-~/Apps/acme/acme.sh --issue --server letsencrypt --home ~/Apps/acme --dns dns_ali \
-  -d "$domain" -d "*.$domain"
-
-cat ~/Apps/acme/account.conf
-# SAVED_Ali_Key='xxxx'
-# SAVED_Ali_Secret='xxxx'
-
-#### 3. setup cron
-# crontab -l; crontab -e
-# 0 0 * * * /path/to/home/crons/acme_cron.sh
