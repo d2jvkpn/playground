970460c72552494cf77af0ae72a1f789ebf9e205
d2jvkpn <d2jvkpn@noreply.local>
2024-04-15 14:25:58 +0800

    ...

---
+++ linux/prototype/_images_to_pdf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+# echo "Hello, world!"
+
+# sudo apt install img2pdf
+
+img2pdf --output document.pdf *.jpg
