73efd07eb254822a777da18da3877627d4e02a3d
d2jvkpn <d2jvkpn@noreply.local>
2024-04-15 14:48:38 +0800

    ...

--- docker/registry/registry-private/docker_deploy.sh
+++ docker/registry/registry-private/docker_deploy.sh
@@ -2,9 +2,11 @@
 set -eu -o pipefail
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-http_port=$1
+http_port=$1; domain=$2
 
-export HTTP_Port=$http_port
+mkdir -p data/registry
+
+export HTTP_Port=$http_port; DOMAIN=$2
 
 envsubst < ${_path}/docker_deploy.yaml > docker-compose.yaml
 
