9353e2377301943c94603657265b14acee0300fb
d2jvkpn <d2jvkpn@noreply.local>
2024-05-01 23:56:31 +0800

    ..

--- linux/shell/api_util.go
+++ linux/shell/api_util.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"net/url"
 	"os"
+	"time"
 
 	"crypto/hmac"
 	"crypto/md5"
@@ -25,8 +27,15 @@ func main() {
 	)
 
 	flag.StringVar(&content, "content", "", "content")
+
 	flag.StringVar(&secret, "secret", "", "secret")
-	flag.StringVar(&method, "method", "", "md5, sha1, sha224, sha256, hmac-sha1, hmac-sha256")
+
+	flag.StringVar(
+		&method, "method", "",
+		"unix, unix-milli, rfc3339, rfc3339-milli, http-date, url-escape, url-unescape, "+
+			"md5, sha1, sha224, sha256, hmac-sha1, hmac-sha256",
+	)
+
 	flag.StringVar(&encode, "encode", "", "base64, hex")
 
 	flag.Parse()
@@ -37,13 +46,24 @@ func main() {
 		}
 	}()
 
-	if content == "" {
-		err = errors.New("invalid conent")
-		return
-	}
-
-	// fmt.Println(time.Now().UTC().Format(time.RFC1123))
 	switch method {
+	case "unix":
+		fmt.Println(time.Now().Unix())
+	case "unix-milli":
+		fmt.Println(time.Now().UnixMilli())
+	case "rfc3339":
+		fmt.Println(time.Now().Format(time.RFC3339))
+	case "rfc3339ms":
+		fmt.Println(time.Now().Format("2006-01-02T15:04:05.000-07:00"))
+	case "http-date":
+		fmt.Println(time.Now().UTC().Format(time.RFC1123))
+	case "url-escape":
+		fmt.Println(url.QueryEscape(content))
+	case "url-unescape":
+		if encode, err = url.QueryUnescape(content); err != nil {
+			return
+		}
+		fmt.Println(encode)
 	case "md5":
 		bts := md5.Sum([]byte(content))
 		ans = bts[:]
@@ -57,21 +77,18 @@ func main() {
 		bts := sha256.Sum256([]byte(content))
 		ans = bts[:]
 	case "hmac-sha1":
-		if secret == "" {
-			err = errors.New("secret is empty")
-			return
-		}
+		/*
+			if secret == "" {
+				err = errors.New("secret is empty")
+				return
+			}
+		*/
 
 		hash := hmac.New(sha1.New, []byte(secret))
 		hash.Write([]byte(content))
 		bts := hash.Sum(nil)
 		ans = bts[:]
 	case "hmac-sha256":
-		if secret == "" {
-			err = errors.New("secret is empty")
-			return
-		}
-
 		hash := hmac.New(sha256.New, []byte(secret))
 		hash.Write([]byte(content))
 
@@ -82,6 +99,10 @@ func main() {
 			err = errors.New("undefined method")
 			return
 		}
+	}
+
+	if len(ans) == 0 {
+		return
 	}
 
 	switch encode {
