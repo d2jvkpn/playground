a583dcf63ae2d6c8568d2b1ec7df7621c2c866f3
d2jvkpn <d2jvkpn@noreply.local>
2024-05-16 06:36:43 +0800

    ...

---
+++ linux/prototype/_sed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+#### print the line at the end of file and append a empty string
+sed -n '$p; $a\ '
