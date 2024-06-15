c5b37b745416245b7777f7de5754a0e1a096081a
d2jvkpn <d2jvkpn@noreply.local>
2024-06-15 09:56:57 +0800

    ...

--- docker/golang/deployments/docker_deploy.sh
+++ docker/golang/deployments/docker_deploy.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-name=collector
-mkdir -p configs logs data/mongo
+command -v yq
 
-export TAG=$1 APP_ENV=$2 PORT=$3
+export IMAGE_Tag=$1 HTTP_Port=$2
+
+export APP_Name=$(yq .app_name project.yaml)
+export IMAGE_Name=$(yq .image_name project.yaml)
+export USER_UID=$(id -u) USER_GID=$(id -g)
+
+mkdir -p configs logs data/$APP_Name data/postgres data/redis
 envsubst < ${_path}/docker_deploy.yaml > docker-compose.yaml
 
-# docker-compose pull
-[[ ! -z "$(docker ps --all --quiet --filter name=${name}_$APP_ENV)" ]] &&
-  docker rm -f ${name}_$APP_ENV
-# docker-compose down for running containers only, not stopped containers
+####
+exit 0
+dry_run=${dry_run:-"false"}
 
+[[ "$dry_run" == "true" ]] && { >&2 echo "exit"; exit 0; }
+# docker-compose pull
+
+>&2 echo "==> docker up"
 docker-compose up -d
+
+####
+exit 0
+container=xxxx
+# 'docker-compose down' removes running containers only, not stopped containers
+[ ! -z "$(docker ps --all --quiet --filter name=$container)" ] && docker rm -f $container
+
+docker stop $container
+docker rm $container
