3ff3200fccd9fb723d80ca70328b2201c9d88726
d2jvkpn <d2jvkpn@noreply.local>
2024-06-18 13:44:42 +0800

    ...

--- docker/postgres/pg_dump.sh
+++ docker/postgres/pg_dump.sh
@@ -4,14 +4,15 @@ _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
 # echo "Hello, world!"
 db=${1:-postgres}
+tag=$(date +%F-%s)
 
 mkdir -p temp
 
 docker exec postgres \
   pg_dump --host=localhost --port=5432 --username=postgres \
   --verbose --format=plain --no-owner \
-  --inserts --rows-per-insert=100 --on-conflict-do-nothing \
-  $db | sed 's/\t/  /g' > temp/$db.sql
+  --inserts --rows-per-insert=100 \
+  $db | sed 's/\t/  /g' > temp/$db.$tag.sql
 
 exit
 docker exec postgres \
