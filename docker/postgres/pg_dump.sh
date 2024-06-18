22878667dc974e9ce86a82938911c10daeb27430
d2jvkpn <d2jvkpn@noreply.local>
2024-06-18 13:40:20 +0800

    ...

--- docker/postgres/pg_dump.sh
+++ docker/postgres/pg_dump.sh
@@ -11,17 +11,18 @@ docker exec postgres \
   pg_dump --host=localhost --port=5432 --username=postgres \
   --verbose --format=plain --no-owner \
   --inserts --rows-per-insert=100 --on-conflict-do-nothing \
-   $db > temp/$db.sql
+  $db | sed 's/\t/  /g' > temp/$db.sql
 
+exit
 docker exec postgres \
   pg_dump --host=localhost --port=5432 --username=postgres \
   --verbose --format=plain --no-owner \
   --schema-only \
-  $db > temp/$db.schema.sql
+  $db | sed 's/\t/  /g' > temp/$db.schema.sql
 
 # --column-inserts, --table $t1 --table $t2
 docker exec postgres \
   pg_dump --host=localhost --port=5432 --username=postgres \
   --verbose --format=plain --no-owner \
   --data-only --inserts --rows-per-insert=100 --on-conflict-do-nothing \
-  $db > temp/$db.data.sql
+  $db | sed 's/\t/  /g' > temp/$db.data.sql
