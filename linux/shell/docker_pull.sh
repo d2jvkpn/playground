05f81bcc601ed3fa04c2be9c6ddff572ec029a3f
d2jvkpn <d2jvkpn@noreply.local>
2024-06-24 08:52:06 +0800

    ...

--- linux/shell/docker_pull.sh
+++ linux/shell/docker_pull.sh
@@ -5,14 +5,20 @@ _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 remote_host=$1
 image=$2
 
+remove=${remove:-false}
+
 base=$(basename $image | sed 's/:/_/g')
 
 ssh $remote_host "set -e; docker pull $image; docker save $image -o $base.tar; pigz $base.tar"
+
 rsync -arvP $remote_host:$base.tar.gz /tmp/
+ssh $remote_host "rm $base.tar.gz"
 
 pigz -dc /tmp/$base.tar.gz | docker load
 
-ssh $remote_host "rm $base.tar.gz; docker rmi $image || true"
+[[ "$remove" == "true" ]] && {
+    ssh $remote_host "docker rmi $image || true";
+    rm /tmp/$base.tar.gz;
+}
 
-rm /tmp/$base.tar.gz
 docker images --quiet --filter "dangling=true" ${image%:*} | xargs -i docker rmi {} || true
