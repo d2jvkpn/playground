193806ea56dbda70c145b21cb122d9ba3686fdf3
d2jvkpn <d2jvkpn@noreply.local>
2024-07-01 23:25:51 +0800

    ...

--- knowledge/database/postgres/join_group_by.sql
+++
@@ -1,6 +0,0 @@
-SELECT t1.org_id, t2.name org_name, array_agg(t1.account_id) account_ids
-FROM user_org_members t1
-INNER JOIN user_orgs t2 ON t1.org_id = t2.id
-WHERE t2.status = true
-GROUP BY t1.org_id, t2.name
-ORDER BY t2.name, t1.org_id;
