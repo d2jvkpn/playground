8725e5949e34a0c2f0ede00a9dbf663b3c5affc5
d2jvkpn <d2jvkpn@noreply.local>
2024-07-04 10:40:40 +0800

    ...

--- knowledge/database/postgres/functions.sql
+++ knowledge/database/postgres/functions.sql
@@ -1,1 +1,25 @@
 SELECT pg_get_functiondef('public.calculate_sales_tax'::regproc);
+
+
+CREATE OR REPLACE FUNCTION update_now() RETURNS trigger AS $$
+BEGIN
+  NEW.updated_at := now();
+  RETURN NEW;
+END;
+$$LANGUAGE plpgsql;
+
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_proc
+    WHERE proname = 'add_numbers'
+  ) THEN
+    CREATE FUNCTION add_numbers(a integer, b integer) RETURNS integer
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT
+    RETURN a + b;
+  END IF;
+END $$;
