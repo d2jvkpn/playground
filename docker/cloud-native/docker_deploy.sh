ea43bcc764a6ebddc94bf1c4708db351e1c24fed
d2jvkpn <d2jvkpn@noreply.local>
2024-07-12 09:00:39 +0800

    ...

--- docker/cloud-native/docker_deploy.sh
+++ docker/cloud-native/docker_deploy.sh
@@ -2,10 +2,14 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
+export USER_UID=$(id -u) USER_GID=$(id -g)
+
 mkdir -p configs logs data/prometheus data/grafana
 
 cp examples/{jaeger.yaml,otel-collector.yaml,prometheus.yaml,prometheus_web.yaml} configs/
-cp docker_deploy.yaml docker-compose.yaml
 
+envsubst > docker-compose.yaml < docker_deploy.yaml
+
+exit
 # docker network create grafana
 docker-compose up -d
---
+++ linux/misc/wpress_file.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+# echo "Hello, world!"
+
+npm install wpress-extract
+
+npx wpress-extract path/to/file.wpress
