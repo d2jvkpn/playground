94887dcd654fae903f15afcc70586f0d3d392faa
d2jvkpn <d2jvkpn@noreply.local>
2024-07-20 20:05:24 +0800

    ...

---
+++ knowledge/database/postgres/latest_data.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT ON (user_id) *
+FROM records
+ORDER BY user_id, timestamp DESC;
+
+SELECT DISTINCT ON (user_id) *
+FROM records
+ORDER BY user_id, timestamp DESC;
+
+SELECT DISTINCT ON (user_id) *
+FROM records
+ORDER BY user_id, timestamp DESC;
+
+SELECT DISTINCT ON (sn) sn, date
+FROM t_inverter_data
+ORDER BY sn, date DESC;
