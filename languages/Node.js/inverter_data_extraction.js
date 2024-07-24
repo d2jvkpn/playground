457ef0e74f2b227995d8414f79247dc18f01f3ef
d2jvkpn <d2jvkpn@noreply.local>
2024-07-25 07:04:57 +0800

    ...

--- languages/Node.js/inverter_data_extraction.js
+++ languages/Node.js/inverter_data_extraction.js
@@ -27,12 +27,3 @@ let result = Array.from({ length: size }, (_, i) => {
 console.log("==> Currents:", currents);
 console.log("==> Voltages:", voltages);
 console.log("==> Result:", result);
-/* ==> Result:
-[
-  { name: '组串1', current: '11.3', voltage: '753.8' },
-  { name: '组串2', current: '10.6', voltage: '761.8' },
-  { name: '组串3', current: '11.0', voltage: '796.6' },
-  { name: '组串4', current: '21.9', voltage: '753.1' },
-  { name: '组串5', current: '0.0', voltage: '0.0' }
-]
-*/
