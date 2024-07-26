6b4441123c0b7626e41df874e125584d223f85d3
d2jvkpn <d2jvkpn@noreply.local>
2024-07-26 19:41:56 +0800

    ...

--- docker/vue-web/deployments/docker_build.sh
+++ docker/vue-web/deployments/docker_build.sh
@@ -10,16 +10,27 @@ DOCKER_Push=${DOCKER_Push:-false}
 
 yaml=${yaml:-${_path}/docker_build.yaml}
 
+# app_name=$(yq -p json -o yaml package.json | yq .name)
+# app_version=$(yq -p json -o yaml package.json | yq .version)
+app_name=$(yq .app_name $yaml)
+app_version=$(yq .version $yaml)
+
 image_name=$(yq .image_name $yaml)
-git_branch=$(yq .$tag.branch $yaml)
-# image_tag=${git_branch}-${app_version}
 image_tag=$(yq .$tag.image_tag $yaml)
 image=$image_name:$image_tag
 
+git_branch=$(yq .$tag.branch $yaml)
+git_commit_id=$(git rev-parse --verify HEAD) # git log --pretty=format:'%h' -n 1
+git_commit_time=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%FT%T%:z)
+git_tree_state="clean"
+uncommitted=$(git status --short)
+unpushed=$(git diff origin/$git_branch..HEAD --name-status)
+# [[ ! -z "$uncommitted$unpushed" ]] && git_tree_state="dirty"
+[[ ! -z "$unpushed" ]] && git_tree_state="unpushed"
+[[ ! -z "$uncommitted" ]] && git_tree_state="uncommitted"
+
 VITE_API_URL=$(yq .$tag.VITE_API_URL $yaml)
 
-app_name=$(yq -p json -o yaml package.json | yq .name)
-app_version=$(yq -p json -o yaml package.json | yq .version)
 
 build_time=$(date +'%FT%T%:z')
 
@@ -33,7 +44,12 @@ EOF
 cat > cache.local/build.yaml << EOF
 app_name: $app_name
 app_version: $app_version
+
 git_branch: $git_branch
+git_commit_id: $git_commit_id
+git_commit_time: $git_commit_time
+git_tree_state: $git_tree_state
+
 build_time: $build_time
 
 VITE_API_URL: $VITE_API_URL
