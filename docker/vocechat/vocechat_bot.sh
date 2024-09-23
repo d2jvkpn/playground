9d5ddcf1cd8c0183dd9d90f2b8d9a7ab94fb18da
d2jvkpn <d2jvkpn@noreply.local>
2024-09-23 18:25:44 +0800

    ...

--- docker/vocechat/vocechat_bot.sh
+++ docker/vocechat/vocechat_bot.sh
@@ -2,12 +2,14 @@
 set -eu -o pipefail
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
+#### 1.
 # command -v jq > /dev/null ||  { echo "can't find jq"; exit 1; }
 command -v yq > /dev/null ||  { echo "can't find yq"; exit 1; }
 
 [[ $# -eq 0 ]] && { >&2 echo "Pass your message as argument(s)!"; exit 1; }
 message="$*"
 
+#### 2.
 yaml=${yaml:-configs/vocechat.yaml}
 server=$(yq .server $yaml)
 api_key=$(yq .bot.api_key $yaml)
@@ -15,18 +17,19 @@ send_to_user=$(yq .bot.send_to_user $yaml)
 
 ua="Mozilla/5.0 Gecko/20100101 Firefox/130.0"
 
-echo ">>> sending to user: $message"
+#### 3.
+echo "==> 1. sending to user: $message"
 
-curl -i -k -X POST -H "User-Agent: $ua" -H "Origin: $server" -H "x-api-key: $api_key" \
-  "$server/api/bot/send_to_user/$send_to_user" \
+curl -i -k -H "User-Agent: $ua" -H "Origin: $server" -H "x-api-key: $api_key" \
+  -X POST "$server/api/bot/send_to_user/$send_to_user" \
   -H 'Content-Type: text/plain' --data-raw "$message"
 
 ####
 exit
 send_to_group=$(yq .bot.send_to_group $yaml)
 
-echo ">>> sending to group: $message"
+echo "==> 2. sending to group: $message"
 
-curl -i -k -X POST -H "User-Agent: $ua" -H "Origin: $server" -H "x-api-key: $api_key"
-  "$server/api/bot/send_to_group/$send_to_group"
+curl -i -k -H "User-Agent: $ua" -H "Origin: $server" -H "x-api-key: $api_key"
+  -X POST "$server/api/bot/send_to_group/$send_to_group"
   -H 'Content-Type: text/plain' --data-raw "$message"
--- docker/vocechat/vocechat_user.sh
+++ docker/vocechat/vocechat_user.sh
@@ -2,35 +2,41 @@
 set -eu -o pipefail
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
+#### 1.
 command -v jq > /dev/null ||  { echo "can't find jq"; exit 1; }
 command -v yq > /dev/null ||  { echo "can't find yq"; exit 1; }
 
 [[ $# -eq 0 ]] && { >&2 echo "Pass your message as argument(s)!"; exit 1; }
 message="$*"
 
+#### 2.
 yaml=${yaml:-configs/vocechat.yaml}
 server=$(yq .server $yaml)
 email=$(yq .account.email $yaml)
 password=$(yq .account.password $yaml)
-send_to_user=$(yq .send_to_user $yaml)
+send_to_user=$(yq .account.send_to_user $yaml)
 
+ua="Mozilla/5.0 Gecko/20100101 Firefox/130.0"
+
+#### 3.
 data=$(
   jq -n --arg email "$email" --arg password "$password" \
     '{credential:{email:$email,password:$password,type:"password"}}'
 )
 
+echo "==> 1. login"
+
 token=$(
-  curl -k -X POST "$server/api/token/login" -H "Referer: $server" \
+  curl -k -H "User-Agent: $ua" -H "Origin: $server" -H "Referer: $server" \
+    -X POST "$server/api/token/login" \
     -H 'Content-Type: application/json' --data-raw "$data" |
     jq -r .token
 )
 
-curl -k -L -X POST "$server/api/user/$send_to_user/send" \
-  -H "X-API-Key: $token"        \
-  -H 'Content-Type: text/plain' \
-  -d "$message"
+#### 4.
+echo "==> 2. sending to user: $message"
 
-curl -k -L -X POST "$server/api/group/$send_to_user/send" \
-  -H "X-API-Key: $token"        \
-  -H 'Content-Type: text/plain' \
-  -d "$message"
+curl -i -k -X POST -H "User-Agent: $ua" -H "Origin: $server" -H "Referer: $server" \
+  -H "X-API-Key: $token"  \
+  -X POST "$server/api/user/$send_to_user/send" \
+  -H 'Content-Type: text/plain' -d "$message"
