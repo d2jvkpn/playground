c536fa108a0fbc43c2acae1aab80706d7a5867ce
d2jvkpn <d2jvkpn@noreply.local>
2024-09-23 23:55:31 +0800

    ...

--- docker/metube/docker_deploy.sh
+++ docker/metube/docker_deploy.sh
@@ -9,3 +9,7 @@ export USER_UID=$(id -u) USER_GID=$(id -g)
 mkdir -p logs configs data/metube data/caddy/data data/caddy/config
 
 envsubst < docker_deploy.yaml > docker-compose.yaml
+
+exit
+
+docker run --rm -it caddy:2-alpine caddy hash-password --plaintext metube
--- docker/vocechat/vocechat_curl.sh
+++ docker/vocechat/vocechat_curl.sh
@@ -2,25 +2,40 @@
 set -eu -o pipefail
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-#### 1.
+#### 1. setup
 command -v jq > /dev/null ||  { echo "can't find jq"; exit 1; }
 command -v yq > /dev/null ||  { echo "can't find yq"; exit 1; }
 
 [[ $# -eq 0 ]] && { >&2 echo "Pass your message as argument(s)!"; exit 1; }
-message="$*"
+msg="$*"
 
-#### 2.
+#### 2. config
 yaml=${yaml:-configs/local.yaml}
 server=$(yq .server $yaml)
-email=$(yq .user.email $yaml)
-password=$(yq .user.password $yaml)
-send_to_user=$(yq .user.send_to_user $yaml)
+
+api_key=$(yq .api_key $yaml)
+
+email=$(yq .email $yaml)
+password=$(yq .password $yaml)
 
 ua="Mozilla/5.0 Gecko/20100101 Firefox/130.0"
 
-#### 3.
-set -x
+#### 3. bot send msg to user
+echo "==> 1. sending to user: $msg"
 
+curl -i -k -H "User-Agent: $ua" -H "Origin: $server" -H "x-api-key: $api_key" \
+  -X POST "$server/api/bot/send_to_user/1" \
+  -H 'Content-Type: text/plain' --data-raw "$msg"
+
+#### 4. bot send msg to group
+exit
+echo "==> 2. sending to group: $message"
+
+curl -i -k -H "User-Agent: $ua" -H "Origin: $server" -H "x-api-key: $api_key"
+  -X POST "$server/api/bot/send_to_group/1"
+  -H 'Content-Type: text/plain' --data-raw "$msg"
+
+#### 4. user login
 data=$(
   jq -n --arg email "$email" --arg password "$password" \
     '{credential:{email:$email,password:$password,type:"password"}}'
@@ -28,7 +43,7 @@ data=$(
 # expired_in: 30
 # refresh_token: xxxx.xxxx.xxxx
 
-echo "==> 1. login"
+echo "==> 3. login"
 
 token=$(
   curl -k -H "User-Agent: $ua" -H "Origin: $server" -H "Referer: $server" \
@@ -37,10 +52,10 @@ token=$(
     jq -r .token
 )
 
-#### 4.
-echo "==> 2. sending to user: $message"
+#### 5. user send msg to user
+echo "==> 2. sending to user: $msg"
 
 curl -i -k -X POST -H "User-Agent: $ua" -H "Origin: $server" -H "Referer: $server" \
   -H "X-API-Key: $token"  \
-  -X POST "$server/api/user/$send_to_user/send" \
-  -H 'Content-Type: text/plain' -d "$message"
+  -X POST "$server/api/user/1/send" \
+  -H 'Content-Type: text/plain' -d "$msg"
