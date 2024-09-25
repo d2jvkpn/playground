fe82d6ad23a9be45411a33b1b668b68774a82610
d2jvkpn <d2jvkpn@noreply.local>
2024-09-25 09:27:55 +0800

    caddy: excalidraw, metube

---
+++ docker/caddy/README.md
@@ -0,0 +1,14 @@
+# Caddy
+---
+
+
+#### C01. chapter1
+1. Apps
+- metube
+- excalidraw
+
+2. world
+
+#### C02. chapter2
+
+#### C03. chapter3
--- docker/caddy/docker_deploy.sh
+++ docker/caddy/docker_deploy.sh
@@ -2,16 +2,24 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-# echo "Hello, world!"
-
 HTTP_Port=${1:-4042}
-
 export USER_UID=$(id -u) USER_GID=$(id -g) HTTP_Port=$HTTP_Port
 
-mkdir -p logs configs data/caddy/data data/caddy/config
+mkdir -p logs configs
+mkdir -p data/caddy/data data/caddy/config data/metube
 
 envsubst < docker_deploy.yaml > docker-compose.yaml
 
 exit
+docker-compose pull
+docker-compose logs
 
-docker run --rm -it caddy:2-alpine caddy hash-password --plaintext account-password
+docker run --detach         \
+  --name excalidraw         \
+  --restart=always          \
+  --publish=${HTTP_Port}:80 \
+  excalidraw/excalidraw:latest
+
+xdg-open http://localhost:4042
+
+docker run --rm -it caddy:2-alpine caddy hash-password --plaintext YourPassword
