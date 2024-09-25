d7947d99bb77e60d2ff155ece196ca54c1beaee8
d2jvkpn <d2jvkpn@noreply.local>
2024-09-25 11:31:52 +0800

    linux/shell/resize_image.sh

---
+++ linux/shell/resize_image.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+# with the help of ChatGPT
+
+longest_edge=${longest_edge:-1980}
+
+if [ $# -eq 0 ]; then
+    >&2 echo -e "Usage:\n    longest_edge=1980 bash resize_image.sh img01.jpeg img02.jpeg"
+    exit 0
+elif [ $# -eq 1 ]; then
+    :
+else
+    for a in "$@"; do
+        bash "$0" "$a"
+    done
+    exit 0
+fi
+
+image_path="$1"
+
+# 获取图片的宽度和高度
+dimensions=$(identify -format "%w %h" "$image_path")
+width=$(echo $dimensions | cut -d ' ' -f 1)
+height=$(echo $dimensions | cut -d ' ' -f 2)
+
+# 确定缩放比例
+if [ "$width" -gt "$height" ]; then
+    # 横向图片
+    resize_dimension="${longest_edge}x"  # 固定宽度
+else
+    # 纵向图片
+    resize_dimension="x${longest_edge}"  # 固定高度
+fi
+
+base=${image_path%.*}
+ext=${image_path##*.}
+output="${base}.resized.${ext}"
+
+>&2 echo "==> longest_edge=$longest_edge, output=$output"
+
+# 进行尺寸调整
+convert "$image_path" -resize "$resize_dimension" "$output"
+
+>&2 echo "==> image has been resized and saved as $output"
