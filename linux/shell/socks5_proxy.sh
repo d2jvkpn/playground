b2084ca229aafb797dcb0fea2feb5a4635d79d18
d2jvkpn <d2jvkpn@noreply.local>
2024-09-28 21:42:29 +0800

    ...

--- linux/shell/socks5_proxy.sh
+++ linux/shell/socks5_proxy.sh
@@ -28,13 +28,13 @@ ssh -NC -D "$address" \
 
 exit 0
 
-https_proxy=socks5://127.0.0.1:1081 git pull
+proxy=socks5://127.0.0.1:1081
+# proxy=socks5://username:password@127.0.0.1:1081
 
-https_proxy=socks5://127.0.0.1:1081 curl -4 https://icanhazip.com
-
-https_proxy=socks5://username:password@127.0.0.1:1081 curl -4 https://icanhazip.com
+https_proxy=$proxy git pull
+https_proxy=$proxy curl -4 https://icanhazip.com
 
 # neither firefox or chromium support socks5 with auth
-chromium --disable-extensions --incognito --proxy-server="socks5://127.0.0.1:1081"
+chromium --disable-extensions --incognito --proxy-server="$proxy"
 
 firefox -p proxy
