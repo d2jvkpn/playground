f84412c7d9bb0afbbf46946841b3ccafb6b1e1e0
d2jvkpn <d2jvkpn@noreply.local>
2024-11-05 16:44:20 +0800

    ...

---
+++ container/openvpn/openvpn.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+# https://github.com/kylemanna/docker-openvpn
+server=$1
+UDP_Port=${2:-1194}
+
+#### 1. initialize
+mkdir -p data/openvpn
+
+docker run --rm -v $PWD/data/openvpn:/etc/openvpn kylemanna/openvpn:latest \
+  ovpn_genconfig -u udp://$server
+
+# -e EASYRSA_KEY_SIZE=4096
+docker run --rm -it -v $PWD/data/openvpn:/etc/openvpn \
+  kylemanna/openvpn:latest ovpn_initpki
+
+# Enter New CA Key Passphrase:
+# Re-Enter New CA Key Passphrase: hello
+# Common Name(...): $server
+# Enter pass phrase for /etc/openvpn/pki/private/ca.key: hello
+
+#### 2. deploy
+export UDP_Port=$UDP_Port
+envsubst < container_deploy.yaml > docker-compose.yaml
+
+#### 3. run
+exit
+docker-compose up -d
+
+#### 4. generate client
+container=$(yq .services.openvpn.container_name docker-compose.yaml)
+account=d2jvkpn
+
+docker exec -it $container easyrsa build-client-full $account nopass
+
+docker run -v $PWD/data/openvpn:/etc/openvpn --rm \
+  kylemanna/openvpn:latest ovpn_getclient $account > data/$account.ovpn
+
+sudo openvpn --config data/$account.ovpn
+# ... Initialization Sequence Completed
+
+docker exec -it $container easyrsa revoke $account
+# docker exec $container ls /etc/openvpn/pki/issued
+docker exec -it $container easyrsa gen-crl
+docker restart $container
+# sudo openvpn --config data/$account.ovpn
+
+#### 5. firewall and network
+sudo ufw allow 1194/udp
+sudo sysctl -w net.ipv4.ip_forward=1
