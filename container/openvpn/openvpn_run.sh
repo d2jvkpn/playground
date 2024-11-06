b5385db6b3350219a3c41f36ac5d13f9547fd237
d2jvkpn <d2jvkpn@noreply.local>
2024-11-06 14:46:41 +0800

    ...

--- container/openvpn/openvpn_run.sh
+++ container/openvpn/openvpn_run.sh
@@ -2,14 +2,14 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-
 vpn_file=${1:-configs/data.ovpn}
 vpn_auth=${2:-configs/openvpn.auth}
 
-cd ${_path}
+cd "${_path}"
 
 sudo openvpn --config "$vpn_file" --auth-user-pass "$vpn_auth"
 
+####
 exit
 
 cat > configs/openvpn.auth <<EOF
