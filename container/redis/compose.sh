fed8ed79bdbc947840e71563a4fad846901d270b
d2jvkpn <d2jvkpn@noreply.local>
2024-11-10 01:13:17 +0800

    ...

--- container/redis/compose.sh
+++ container/redis/compose.sh
@@ -4,19 +4,20 @@ _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
 
 REDIS_Port=${1:-6379}
-mkdir -p configs data/redis
+mkdir -p data/redis
+
 touch data/redis/aclfile.acl
 
 password=$(tr -dc 'A-Za-z0-9!@#$%^&*()' < /dev/urandom | head -c 24 || true)
 
 [ -f configs/redis.password ] || echo "$password" > configs/redis.password
 
-[ -f configs/redis.conf ] || \
-cat > configs/redis.conf <<EOF
+[ -f data/redis/redis.conf ] || \
+cat > data/redis/redis.conf <<EOF
 requirepass "$password"
 
-logfile /app/data/redis-server.log
-dir /app/data
+logfile /data/redis-server.log
+dir /data
 dbfilename dump.rdb
 
 aof-use-rdb-preamble yes
@@ -26,6 +27,6 @@ io-threads-do-reads yes
 EOF
 
 export USER_UID=$(id -u) USER_GID=$(id -g) REDIS_Port=$REDIS_Port
-envsubst < docker_deploy.yaml > docker-compose.yaml
+envsubst < containers.yaml > docker-compose.yaml
 
 docker-compose up -d
