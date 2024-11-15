e26585858ae33fdd82df7934601f9b098118fc33
d2jvkpn <d2jvkpn@noreply.local>
2024-11-15 17:20:43 +0800

    ...

--- container/docker_tools.sh
+++ container/docker_tools.sh
@@ -6,3 +6,10 @@ pip3 install runlike
 pip3 install whaler
 
 docker ps | awk 'NR>1{print $NF}' | xargs -i runlike {}
+
+exit
+
+docker ps |
+  awk 'NR>1{print $NF}' |
+  xargs -i docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+  assaflavie/runlike {}
