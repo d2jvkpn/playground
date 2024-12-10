3074b8a696a28b3331b7b723d62c50adf2c7e60b
d2jvkpn <d2jvkpn@noreply.local>
2024-12-10 14:24:11 +0800

    ...

---
+++ linux/prototype/_args_to_batch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu -o pipefail # -x
+_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+
+[ $# -eq 0 ] && { >&2 echo '!!!'" no args"; exit 1; }
+
+args=("$@")
+batch_args=$(printf "{%s}" "$(IFS=,; echo "${args[*]}")")
+
+echo target/"${batch_args}"
+
+eval echo target/"${batch_args}"
