9f09bf70ddab4ccd394096dec581f07b40e5475c
d2jvkpn <d2jvkpn@noreply.local>
2024-12-11 16:12:01 +0800

    ...

--- cloud/aliyun/_oss_batch_upload.sh
+++ cloud/aliyun/_oss_batch_upload.sh
@@ -3,25 +3,25 @@ set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
 # mkdir -p 2024-12-06
-# ln -rs /mnt/vdc1/bigdata/{vw_campaign.dump,vw_dashboard.dump,vw.dump,vw_monthly_report.dump} 2024-12-06/
-# nohup ossutil64 cp -r 2024-12-06  oss://archive-2024/adb_pg_database_backup/ &
+# ln -rfs /mnt/vdb1/account/postgres.dump 2024-12-06/
+# nohup ossutil64 cp -r 2024-12-06  oss://bucket/postgres_backup/ &
 
 [ $# -eq 0 ] && { >&2 echo '!!!' "no args: filenames"; exit 1; }
 
-args=("$@")
-batch_args=$(printf "{%s}" "$(IFS=,; echo "${args[*]}")")
+files=$@
 
 tag=$(date +%F)
-source_dir=/path/to/local_dir
 oss_dir=oss://bucket/path/to/remote_dir
+start_at=$(date +%FT%T%:z)
 
-eval ls -lh $source_dir/$batch_args
+echo "==> oss_batch_upload.sh: $start_at"
+ls -lh "$files"
 
 mkdir -p $tag
-eval ln -rfs $source_dir/$batch_args $tag/
+ln -rfs "$files" $tag/
 
 {
-    echo "==> $(date +%FT%T%:z) start: files=$source_dir/$batch_args, oss_dir=$oss_dir/"
+    echo "==> $start_at start: files=$files, oss_dir=$oss_dir/"
     ossutil64 cp -r $tag $oss_dir/
     echo "<== $(date +%FT%T%:z) end"
 } &> ossutil64.${tag}S$(date +%s).log
