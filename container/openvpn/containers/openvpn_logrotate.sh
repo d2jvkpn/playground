f93abf963d5fb7545190519d6495e4b8d3740215
d2jvkpn <d2jvkpn@noreply.local>
2024-12-23 21:38:52 +0800

    ...

--- container/openvpn/containers/openvpn_logrotate.sh
+++ container/openvpn/containers/openvpn_logrotate.sh
@@ -2,9 +2,8 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0)
 
-# echo "5 0 1 * * bash /apps/target/openvpn_log.sh" > temp.crontab
-# crontab temp.crontab
-# rm temp.crontab
+# echo "5 0 1 * * bash /apps/target/openvpn_log.sh" > /tmp/crontab
+# crontab /tmp/crontab
 
 # crond -l 0 -f -L /apps/logs/crond.log
 
