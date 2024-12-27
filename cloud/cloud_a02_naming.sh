6f240ed8aa80da708a4e2ea14eb11fbef09573b7
d2jvkpn <d2jvkpn@noreply.local>
2024-12-27 18:17:51 +0800

    ...

--- cloud/cloud_a02_naming.sh
+++ cloud/cloud_a02_naming.sh
@@ -3,9 +3,9 @@ set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0)
 
 
-echo "aliyun.com: ali-{project}--[region][sn]"
-echo "cloud.tencent.com: tce-{project}--[region][sn]"
-echo "aws.amazon.com: aws-{project}--[region][sn]"
+echo "aliyun.com: ali-{project}-[region][sn]"
+echo "cloud.tencent.com: tce-{project}-[region][sn]"
+echo "aws.amazon.com: aws-{project}-[region][sn]"
 
-echo "aws-ai--sh-01"
-echo "aws-un--sh-01"
+echo "aws-ai-sh01"
+echo "aws-un-sh01"
---
+++ network/openssl/ssl_view.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0) # set -x
+
+domain=$1
+host=${2:-$domain}
+
+echo |
+  openssl s_client -connect $host:443 -servername $domain 2>/dev/null |
+  openssl x509 -noout -subject -dates
+
+# openssl x509 -noout -text
+
+exit
+#### view file
+openssl x509 -in domain.crt -noout -text
+openssl x509 -in domain.crt -noout -subject -dates
+
+exit
+#### view the service
+echo | openssl s_client -connect localhost:443 -servername $domain |
+  openssl x509 -noout -subject -dates
