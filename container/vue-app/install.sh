3e60d5759cc58833277afa22b48a05b0d0667966
d2jvkpn <d2jvkpn@noreply.local>
2025-01-03 17:07:53 +0800

    ...

--- container/vue-app/install.sh
+++ container/vue-app/install.sh
@@ -3,6 +3,9 @@ set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
 npm install --global serve @vue/cli # version 3.2.13
 
-vue create vue-app
+vue create vue-app --default --packageManager=npm
 
 cd vue-app
+
+npm install
+npm run dev
