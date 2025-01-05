8726d0e025b477b9408b9a373be939a939c3bcae
d2jvkpn <d2jvkpn@noreply.local>
2025-01-05 22:19:44 +0800

    ...

--- network/openssl/ssl_expiry.sh
+++ network/openssl/ssl_expiry.sh
@@ -14,15 +14,19 @@ else
     exit 0
 fi
 
-
 host=$(echo $1 | sed 's#.*//##; s#/.*##')
-port=${port:-443}
+
+if [[ "$host" == *":"* ]]; then
+    addr=$host
+else
+    addr=$host:${port:-443}
+fi
 
 # openssl x509 -noout -subject -dates
 # date -u -d "${expiry_date}" +"%Y-%m-%dT%H:%M:%SZ"
 output=$(
   echo |
-  openssl s_client -connect $host:$port 2> /dev/null |
+  openssl s_client -connect $addr 2> /dev/null |
   openssl x509 --noout --enddate 2>&1 |
   head -n 1 || true
 )
