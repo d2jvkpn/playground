3cc5485207aca604dc5a18262abf79dc12a13a1d
d2jvkpn <d2jvkpn@noreply.local>
2025-01-07 21:31:34 +0800

    container/ubuntu-ssh

---
+++ container/ubuntu-ssh/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
+
+docker build -f Containerfile -t ubuntu-ssh:local ./
+
+for img in $(docker images --filter=dangling=true --filter=label=app=ubuntu-ssh --quiet); do
+    >&2 echo "==> remove image: $img"
+    docker rmi $img || true
+done
