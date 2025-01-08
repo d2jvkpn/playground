7094662a377b78b0e57ddefe0b101f300c17f007
d2jvkpn <d2jvkpn@noreply.local>
2025-01-08 13:52:02 +0800

    ...

--- container/alpine/build.sh
+++ container/alpine/build.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
-set -eu -o pipefail
-_wd=$(pwd); _path=$(dirname $0)
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
-# set -x
-BUILD_Region=${BUILD_Region:-""}
+#### 1. 
+app_name=alpine
+region=${region:-""}
 DOCKER_Push=${DOCKER_Push:-true}
-BUILD_Time=$(date +'%FT%T%:z')
 
 image_name=${1:-registry.cn-shanghai.aliyuncs.com/d2jvkpn/alpine}
 image_tag=${2:-3}
 
 image=$image_name:$image_tag
 
-####
+#### 2. 
 # docker pull alpine:3
 
 docker build --no-cache \
-  --build-arg=BUILD_Region="$BUILD_Region" \
-  --build-arg=BUILD_Time="$BUILD_Time" \
+  --build-arg=APP_Name=$app_name \
+  --build-arg=APP_Version=$image_tag \
+  --build-arg=region="$region" \
   -f ${_path}/Containerfile -t $image ./
 
 ####
-docker images -q -f dangling=true $image_name | xargs -i docker rmi {}
-
 # echo "Push image: $image? (yes/no)"
 # read -t 5 ans || true
 # [ "$ans" != "yes" ] && exit 0
 
 # DOCKER_Push=$(printenv DOCKER_Push || true)
-[ "$DOCKER_Push" == "false" ] && exit 0
-docker push $image
+[ "$DOCKER_Push" != "false" ] && docker push $image
+
+docker images -q -f dangling=true $image_name | xargs -i docker rmi {}
+
+for img in $(docker images --filter=dangling=true --filter=label=app=$app_name --quiet); do
+    >&2 echo "==> remove image: $img"
+    docker rmi $img || true
+done
