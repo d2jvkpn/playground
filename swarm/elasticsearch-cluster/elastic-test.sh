270b97f2ebac72fab731767a14b197e1c006f160
d2jvkpn <d2jvkpn@noreply.local>
2025-01-13 13:59:07 +0800

    ...

--- swarm/elasticsearch-cluster/elastic-test.sh
+++ swarm/elasticsearch-cluster/elastic-test.sh
@@ -48,6 +48,5 @@ curl $auth "$addr/idx-test/_search?q=content:test" -f
 echo
 
 echo -e "######## nodes"
-curl $auth "$addr/_cat/nodes?v=true&format=yaml" -f |
-tee configs/nodes.yaml
+curl $auth "$addr/_cat/nodes?v=true&format=yaml" -f
 echo
