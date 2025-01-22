245df37ba8f6a2abbfbef5ff5ac2b843bd56c487
d2jvkpn <d2jvkpn@noreply.local>
2025-01-22 10:35:45 +0800

    ...

--- databases/rabbitmq/compose.sh
+++ databases/rabbitmq/compose.sh
@@ -5,14 +5,20 @@ set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
 image=$(yq .services.rabbitmq.image compose.template.yaml)
 
-export USER_UID=$(id -u) USER_GID=$(id -g)
-
 mkdir -p data/rabbitmq configs/rabbitmq
+
+if [ ! -s configs/rabbitmq.pass ]; then
+    password=$(tr -dc '0-9a-zA-Z' < /dev/urandom | fold -w 24 | head -n1 || true)
+    echo $password > configs/rabbitmq.pass
+fi
+password=$(cat configs/rabbitmq.pass)
+
+export USER_UID=$(id -u) USER_GID=$(id -g) PASSWORD=$password
 
 docker run --rm -v $PWD/configs/rabbitmq:/tmp/rabbitmq $image \
   bash -c "cp -r /etc/rabbitmq/* /tmp/rabbitmq && chown -R $USER_UID:$USER_GID /tmp/rabbitmq"
 
-  envsubst < compose.template.yaml > compose.yaml
+envsubst < compose.template.yaml > compose.yaml
 
 docker-compose -f compose.yaml up -d
 
