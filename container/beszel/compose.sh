c28b99ad965fc680df97456da08ba09c26624cb0
d2jvkpn <d2jvkpn@noreply.local>
2025-01-24 00:38:28 +0800

    ...

--- container/beszel/compose.sh
+++ container/beszel/compose.sh
@@ -2,11 +2,18 @@
 set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
 
+HTTP_Port=${1:-8090}
 mkdir -p configs data/beszel
 
-export USER_UID=$(id -u) USER_GID=$(id -g)
+export USER_UID=$(id -u) USER_GID=$(id -g) \
+  HTTP_Port=$HTTP_Port
 
 envsubst < compose.template.yaml > compose.yaml
+
+echo -e "\ncompose.yaml"
+echo '```yaml'
+cat compose.yaml
+echo '```'
 
 exit
 docker-compose up -d
