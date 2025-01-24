0911b876892ffd60bdc42375a6bdfedc3db49577
d2jvkpn <d2jvkpn@noreply.local>
2025-01-24 17:24:18 +0800

    ...

--- databases/rabbitmq-cluster/compose.init.sh
+++ databases/rabbitmq-cluster/compose.init.sh
@@ -12,9 +12,11 @@ function check_value() {
 
 mkdir -p data/rabbitmq-node{01..03}
 
-export RABBITMQ_Cookie=$(yq .rabbitmq.cookie configs/rabbitmq.yaml)
-export RABBITMQ_User=$(yq .rabbitmq.user configs/rabbitmq.yaml)
-export RABBITMQ_Password=$(yq .rabbitmq.password configs/rabbitmq.yaml)
+export USER_UID=$(id -u) USER_GID=$(id -g)
+
+export RABBITMQ_Cookie=$(yq .rabbitmq.cookie configs/rabbitmq.yaml) \
+  RABBITMQ_User=$(yq .rabbitmq.user configs/rabbitmq.yaml) \
+  RABBITMQ_Password=$(yq .rabbitmq.password configs/rabbitmq.yaml)
 
 check_value rabbitmq.cookie "$RABBITMQ_Cookie"
 check_value rabbitmq.user "$RABBITMQ_User"
--- databases/rabbitmq-cluster/compose.up.sh
+++ databases/rabbitmq-cluster/compose.up.sh
@@ -6,14 +6,16 @@ ls compose.yaml > /dev/null
 
 docker-compose up -d
 
+sleep 5
+
 docker exec -it rabbitmq-node02 bash -c '
-rabbitmqctl stop_app
-rabbitmqctl join_cluster rabbit@rabbitmq-node01
+rabbitmqctl stop_app &&
+rabbitmqctl join_cluster rabbit@rabbitmq-node01 &&
 rabbitmqctl start_app
 '
 
 docker exec -it rabbitmq-node03 bash -c '
-rabbitmqctl stop_app
-rabbitmqctl join_cluster rabbit@rabbitmq-node01
+rabbitmqctl stop_app &&
+rabbitmqctl join_cluster rabbit@rabbitmq-node01 &&
 rabbitmqctl start_app
 '
