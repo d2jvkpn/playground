b41e17c463bc4d82d35125d337a0aface280dfe2
d2jvkpn <d2jvkpn@noreply.local>
2025-02-22 21:45:58 +0800

    ...

--- ai/grooking_deep_learning/ch09/ch09_09.py
+++ ai/grooking_deep_learning/ch09/ch09_09.py
@@ -13,14 +13,18 @@ from keras.datasets import mnist
 
 random_seed = 1
 batch_size = 100
-alpha = 0.001
-iterations = 5000  # 1000, 2000, 5000
+alpha = 5  # ???
+iterations = 500  # 500, 1000, 2000, 5000
 hidden_size = 256 # 128, 256
 
 np.random.seed(random_seed)
 
-relu = lambda x: (x>=0) * x     # returns x if x > 0, return 0 otherwise
-relu2deriv = lambda y: y>=0  # returns 1 for input > 0, return 0 otherwise
+tanh = lambda x: np.tanh(x)
+tanh2deriv = lambda y: 1 - y**2
+
+def softmax(x):
+     val = np.exp(x)
+     return val / np.sum(val, axis=1, keepdims=True)
 
 def one_hot_labels(labels): # [2] -> [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]]
     result = np.zeros((len(labels),10))
@@ -54,8 +58,9 @@ test_labels = one_hot_labels(y_test[0:test_size])
 
 #### 2. Trainning the neural network
 # Neural Network: layer_0 * weights_0_1 => layer_1 * weights_1_2 => layer_2
-weights_0_1 = 0.2*np.random.random((pixels, hidden_size)) - 0.1 # [-0.1, 0.1]
-weights_1_2 = 0.2*np.random.random((hidden_size, 10)) - 0.1       # [-0.1, 0.1]
+
+weights_0_1 = 0.02*np.random.random((pixels, hidden_size)) - 0.01 # [-0.01, 0.01] # ???
+weights_1_2 = 0.2*np.random.random((hidden_size, 10)) - 0.1           # [-0.1, 0.1]
 trainning_steps = []
 
 t1 = datetime.now().astimezone()
@@ -71,39 +76,38 @@ for n in range(iterations):
         layer_0 = train_inputs[batch_start:batch_end]
         goal = train_labels[batch_start:batch_end]
 
-        layer_1 = relu(np.dot(layer_0, weights_0_1))
-        # dropout_mask = np.random.randint(2, size=layer_1.shape)
+        layer_1 = tanh(np.dot(layer_0, weights_0_1))
         dropout_mask = np.random.choice([0, 1], size=layer_1.shape, p=[0.5, 0.5])
         layer_1 *= (dropout_mask / 0.5)
 
-        layer_2 = np.dot(layer_1, weights_1_2)
+        layer_2 = softmax(np.dot(layer_1, weights_1_2))
 
-        delta_2 = (layer_2 - goal)  # predication - target
-        delta_1 = np.dot(delta_2, weights_1_2.T)  * relu2deriv(layer_1)
+        delta_2 = (layer_2 - goal)  / (layer_0.shape[0] * layer_2.shape[0]) # ???
+        delta_1 = np.dot(delta_2, weights_1_2.T)  * tanh2deriv(layer_1)
         delta_1 *= dropout_mask
 
-        train_error += np.sum(delta_2** 2)
+        # no train_error here
         equals = np.argmax(layer_2, axis=1) == np.argmax(goal, axis=1)
         correct_cnt += np.sum(equals.astype(int))
 
         weights_1_2 -= np.dot(layer_1.T, delta_2) * alpha
         weights_0_1 -= np.dot(layer_0.T, delta_1) * alpha
 
-    train_error, train_acc = train_error/train_size, correct_cnt/train_size
+    train_acc =correct_cnt/train_size
 
     if n%10 == 0 or n == iterations-1:
         layer_0, goal = test_inputs, test_labels
 
-        layer_1 = relu(np.dot(layer_0, weights_0_1))
+        layer_1 = tanh(np.dot(layer_0, weights_0_1))
         layer_2 = np.dot(layer_1, weights_1_2)
 
-        test_error = np.sum((goal - layer_2) ** 2)
+       # no test_error here
         equals = np.argmax(layer_2, axis=1) == np.argmax(goal, axis=1)
         correct_cnt = np.sum(equals.astype(int))
-        test_error, test_acc = test_error/test_size, correct_cnt/test_size
+        test_acc = correct_cnt/test_size
 
-        trainning_steps.append((n, train_error, train_acc, test_error, test_acc))
-        stdout.write(f"--> I{n:04d}: train_error={train_error:.6f}, train_accuracy={train_acc:.3f}, test_error={test_error:.6f}, test_accuracy={test_acc:.3f}\n")
+        trainning_steps.append((n, train_acc, test_acc))
+        stdout.write(f"--> I{n:04d}: train_accuracy={train_acc:.3f}, test_accuracy={test_acc:.3f}\n")
 
 t2 = datetime.now().astimezone()
 
@@ -116,7 +120,7 @@ wts_1_2 = pl.from_numpy(weights_1_2)
 trainning_steps = pl.from_records(
   trainning_steps,
   orient="row",
-  schema=["iteration", "train_error", "train_accuracy", "test_error", "test_accuracy"],
+  schema=["iteration", "train_accuracy", "test_accuracy"],
 )
 
 parameters = {
@@ -125,7 +129,7 @@ parameters = {
   "alpha": 0.001,
   "iterations": 500,
   "hidden_size": 120,
-  "activation_functions": ["relu"],
+  "activation_functions": ["tanh", "softmax"],
   "train_size": train_size,
   "test_size": test_size,
   "start_at": t1.isoformat('T'),
