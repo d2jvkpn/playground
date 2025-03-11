0af29ccec00088e88e8386301baeaccee595b878
d2jvkpn <d2jvkpn@noreply.local>
2025-03-11 12:57:53 +0800

    ...

--- kubernetes/k8s_scripts/kube_ingress_expiry.sh
+++ kubernetes/k8s_scripts/kube_ingress_expiry.sh
@@ -8,6 +8,11 @@ function tls_expiry() {
     namespace=$1; ingress=$2
     secret_names=$(kubectl -n $namespace get ingress $ingress -o jsonpath="{.spec.tls[*].secretName}")
 
+    hosts=$(
+      kubectl -n $namespace get ingress $ingress -o jsonpath="{.spec.tls[*].hosts}" |
+      jq -r '. | join(",")'
+    )
+
     for name in $secret_names; do
         tls_crt=$(kubectl -n $namespace get secret $name -o jsonpath="{.data.tls\.crt}")
 
@@ -16,14 +21,14 @@ function tls_expiry() {
         fi
 
         tls_crt=$(echo $tls_crt | base64 --decode)
-
         subject=$(echo "$tls_crt" | openssl x509 -noout -subject | sed 's/.* = //')
+
         expire_date=$(echo "$tls_crt" | openssl x509 -noout -enddate | cut -d= -f 2)
         expire_rfc3339=$(date -d "$expire_date" +"%Y-%m-%dT%H:%M:%S%:z")
-        t1=$(date -d "$expire_date" +"%s")
-        expire_secs=$((t1 - now_ts))
+        expire_ts=$(date -d "$expire_date" +"%s")
+        expire_secs=$((expire_ts - now_ts))
         
-        echo "$namespace $ingress $subject $expire_rfc3339 $expire_secs"
+        echo "$namespace $ingress $hosts $subject $expire_rfc3339 $expire_secs"
     done
 }
 
@@ -36,7 +41,7 @@ else
     ingress_list=$(kubectl -n $namespace get ingress | awk 'NR>1{print $1}')
 fi
 
-echo "namespace ingress subject expire_rfc3339 expire_secs"
+echo "namespace ingress hosts subject expire_rfc3339 expire_secs"
 for ingress in $ingress_list; do
     tls_expiry $namespace $ingress
 done
