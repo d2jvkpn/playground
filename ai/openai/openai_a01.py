be07fa5bcee96d95523a09b132debdcbb9410bcc
d2jvkpn <d2jvkpn@noreply.local>
2025-03-18 11:14:46 +0800

    ...

--- ai/openai/openai_a01.py
+++ ai/openai/openai_a01.py
@@ -10,11 +10,12 @@ parser = argparse.ArgumentParser(description="parse commandline arguments")
 #parser.add_argument("config", help="config path", default="configs/dev.yaml")
 
 parser.add_argument("-c", "--config", help="config path", default="configs/dev.yaml")
-parser.add_argument("-m", "--model", help="model name: gpt-3.5-turbo, gpt-4o", default="gpt-4o")
-parser.add_argument("-n", "--max_tokens", help="max tokens", type=int, default=1000)
 parser.add_argument("-v", "--verbose", help="verbose", action="store_true")
 
-parser.add_argument('msg', help="message", nargs='*')
+parser.add_argument("--model", help="model name: gpt-3.5-turbo, gpt-4o", default="gpt-4o")
+parser.add_argument("--max_tokens", help="max tokens", type=int, default=1000)
+#parser.add_argument('msg', help="message", nargs='*')
+parser.add_argument('--content', help="message content", default="Hello")
 
 args = parser.parse_args()
 
@@ -23,7 +24,6 @@ with open(args.config, 'r') as f:
     configs = yaml.safe_load(f)
 
 os.environ["https_proxy"] = configs.get("https_proxy", "")
-content = "Hello!" if len(args.msg) == 0 else " ".join(args.msg)
 
 api_key = configs["openai"].get("api_key", "")
 assert(api_key != "")
@@ -31,13 +31,13 @@ assert(api_key != "")
 ##### 3.
 client = OpenAI(api_key=api_key)
 
-print(f"QUESTION: {content}")
+print(f"QUESTION: {args.content}")
 
 response = client.chat.completions.create(
   model = args.model,
   max_tokens = args.max_tokens,
   messages = [
-    { "role": "user", "content": content },
+    { "role": "user", "content": args.content },
   ],
 )
 
