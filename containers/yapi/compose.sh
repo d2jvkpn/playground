297577b0a7e5f35f136cb635e6224b5035064c09
d2jvkpn <d2jvkpn@noreply.local>
2025-03-20 11:02:26 +0800

    ...

--- containers/yapi/compose.sh
+++ containers/yapi/compose.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -eu -o pipefail
-_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
 HTTP_Port=${1:-3000}
 
 mkdir -p data/mongo
 
-HTTP_Port=$HTTP_Port envsubst < docker_deploy.yaml > docker-compose.yaml
+HTTP_Port=$HTTP_Port envsubst < compose.apps.yaml > compose.yaml
 
 # docker-compose pull
 docker-compose up -d
