f0f9086b6ec59cc3342febd1fc948847f02a4e96
d2jvkpn <d2jvkpn@noreply.local>
2025-04-01 19:35:18 +0800

    ...

--- containers/docker_runlike.sh
+++ containers/docker_runlike.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0) # set -x
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
 host=$1
 
 runlike=${runlike:-.local/bin/runlike}
-# docker run --rm -v /var/run/docker.sock:/var/run/docker.sock assaflavie/runlike
+# docker run --rm -v /var/run/docker.sock:/var/run/docker.sock assaflavie/runlike:latest
 
 out_dir=runlike_$(date +%F)
 mkdir -p $out_dir
 
-containers=$(ssh $host docker ps | awk 'NR>1{print $NF}')
+# -o RemoteCommand=none
+ssh  $host docker ps > $out_dir/containers.txt
+
+containers=$(awk 'NR>1{print $NF}' $out_dir/containers.txt)
 
 for container in $containers; do
-    echo "==> container: $container"
+    echo "==> Container: $container"
     ssh $host $runlike -p $container > $out_dir/$container.runlike.sh
 done
 
