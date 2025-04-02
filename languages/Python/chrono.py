d3f5aa69aeb090474d1fc33f3fb086da2e74adb3
d2jvkpn <d2jvkpn@noreply.local>
2025-04-02 18:26:20 +0800

    ...

---
+++ languages/Python/chrono.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from datetime import datetime, timedelta
+from typing import Union, Any, Self
+
+
+class Chrono(object):
+    def __init__(self, t: datetime = None):
+        if t is None:
+            self.at = datetime.now().astimezone()
+        else:
+            self.at = t.astimezone()
+
+    def __repr__(self):
+        return f"{self.at.isoformat('T')}"
+
+    def __str__(self):
+        return f"{self.at.isoformat('T')}"
+
+    def __add__(self, delta: timedelta) -> Self:
+        return Chrono(self.at + delta)
+
+    def __sub__(self, t: Union[Self, timedelta, datetime]) -> Union[Self, timedelta]:
+        if isinstance(t, timedelta):
+            return Self(self.at - t)
+        elif isinstance(t, Self):
+            return self.at - t.at
+        elif isinstance(t, datetime):
+            return self.at - t
+        else:
+            raise ValueError("t must be a timedelta or datetime")
