9544cf422cfec09a2f0fbf90e2a971249340607f
d2jvkpn <d2jvkpn@noreply.local>
2025-04-02 19:01:16 +0800

    ...

---
+++ languages/Python/_signal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys, signal
+
+def handler(sig, frame):
+    print(f"\n!!! Catched signal: signal={sig}, frame={frame}")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
+
+print('Press Ctrl+C')
+signal.pause()
