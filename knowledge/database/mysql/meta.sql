8ffe9a6a97e16815056f4fc2c755536cf2122a7c
d2jvkpn <d2jvkpn@noreply.local>
2025-04-02 18:01:31 +0800

    ...

--- knowledge/database/mysql/meta.sql
+++ knowledge/database/mysql/meta.sql
@@ -1,37 +1,32 @@
 SET @name = 'wowebsitedbp01';
 
+
 -- SHOW TABLE STATUS;
+
 
 SELECT TABLE_NAME, ENGINE, CREATE_TIME, UPDATE_TIME
   FROM INFORMATION_SCHEMA.TABLES
   WHERE TABLE_SCHEMA = @name;
 
 
-SELECT 
-  MIN(CREATE_TIME) AS estimated_db_creation_time
-FROM
-  INFORMATION_SCHEMA.TABLES
-WHERE
-  TABLE_SCHEMA = @name;
+SELECT MIN(CREATE_TIME) AS estimated_db_creation_time
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = @name;
 
-SELECT 
+
+SELECT
   table_schema,
   ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS 'size_mb'
-FROM 
-  information_schema.TABLES
-WHERE 
-  table_schema = @name
-GROUP BY 
-  table_schema;
+FROM information_schema.TABLES
+WHERE table_schema = @name
+GROUP BY table_schema;
 
-SELECT 
+
+SELECT
   table_name,
   ROUND(data_length / 1024 / 1024, 2) AS 'data_mb',
   ROUND(index_length / 1024 / 1024, 2) AS 'index_mb',
   table_rows
-FROM 
-  information_schema.TABLES
-WHERE 
-  table_schema = @name
-ORDER BY 
-  (data_length + index_length) DESC;
+FROM information_schema.TABLES
+WHERE table_schema = @name
+ORDER BY (data_length + index_length) DESC;
