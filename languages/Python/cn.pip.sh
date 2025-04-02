38d5863cedcd6d3d6b31c40ec91825e610894bed
d2jvkpn <d2jvkpn@noreply.local>
2025-04-02 11:03:04 +0800

    ...

--- languages/Python/cn.pip.sh
+++ languages/Python/cn.pip.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail # -x
-_wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
 
 pip3 config set global.index-url 'https://pypi.tuna.tsinghua.edu.cn/simple'
 pip3 config set install.trusted-host 'pypi.tuna.tsinghua.edu.cn'
@@ -11,28 +10,15 @@ python3 -m pip install --upgrade pip
 cat > ~/.config/pip/pip.conf <<EOF
 [global]
 index-url = https://pypi.tuna.tsinghua.edu.cn/simple
-target = ~/.local/lib/python3.10/site-packages/
+#target = ~/.local/lib/python3.10/site-packages/
 
 [install]
 trusted-host = pypi.tuna.tsinghua.edu.cn
 EOF
 
-
 exit
-
 pip3 config set global.index-url 'https://pypi.douban.com/simple/'
 pip3 config set install.trusted-host 'pypi.douban/simple'
 
 pip3 config list
 pip3 install pandas numpy polars
-
-exit
-# /etc/pip.conf
-cat > ~/.config/pip/pip.conf <<EOF
-[global]
-index-url = https://pypi.douban.com/simple/
-target = ~/.local/lib/python3.10/site-packages/
-
-[install]
-trusted-host = pypi.douban/simple
-EOF
