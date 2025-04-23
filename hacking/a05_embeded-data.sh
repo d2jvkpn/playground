d27194df859d5d913a582257d021aaea4d1aaa95
d2jvkpn <d2jvkpn@noreply.local>
2025-04-23 20:25:03 +0800

    ...

--- hacking/a05_embeded-data.sh
+++ hacking/a05_embeded-data.sh
@@ -2,18 +2,16 @@
 set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 
 
-filepath=${2:-$0}
 function extract_embeded() {
     key=$1
-    sed -n "/^__$key-0__$/,/^__$key-1__/p" "$filepath" | tail -n +2 | head -n -1
+    sed -n "/^__${key}-0__$/,/^__${key}-1__/p" "$0" | tail -n +2 | head -n -1
 }
-
-# ....
 
 key=${1:-DATA}
 
-echo "==> Extract embeded from $filepath: key=$key"
+echo "==> Extract embeded: key=$key"
 extract_embeded $key
+# ....
 
 exit 0
 
