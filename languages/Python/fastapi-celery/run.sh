fcc917155bbe428f9a741ce8eafb9b36a22fc2df
d2jvkpn <d2jvkpn@noreply.local>
2025-06-01 08:35:10 +0800

    ...

--- languages/Python/fastapi-celery/run.sh
+++ languages/Python/fastapi-celery/run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 
+####
+python3 -m venv cache/local.venv
 
+source cache/local.venv/bin/activate
+
+pip3 install -r requirements.txt
+
+exit
+####
 export config=configs/local.yaml
 
-uvicorn main:app --host 0.0.0.0 --port 5000 --reload
-
+uvicorn main:app --reload --host=127.0.0.1 --port=5000
 
 celery -A tasks worker --loglevel=info --concurrency=2
 
 exit
+####
 curl -X POST "http://127.0.0.1:5000/upload" -F "file=@data/demo.pdf"
 
 curl -X GET "http://127.0.0.1:5000/task/de406724-e254-406c-92ad-441696035a09"
