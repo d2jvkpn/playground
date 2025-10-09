d86193c8f279601c82398b09243b3c09cf003234
d2jvkpn <d2jvkpn@noreply.local>
2025-10-09 21:49:53 +0800

    docker_save.sh

--- linux/shell/docker_save.sh
+++ linux/shell/docker_save.sh
@@ -3,6 +3,7 @@ set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 
 
 ####
+pull=${pull:-false}
 remove=${remove:-false}
 
 image=$1
@@ -15,10 +16,18 @@ if [[ "$image" != *":"* ]]; then
 fi
 
 ####
-echo "--> exporting $image"
-docker save "$image" -o "$name".tar
+if [[ "$pull" == "true" ]]; then
+    echo "--> pulling $image"
+    docker pull "$image"
+fi
 
-pigz -f "$name".tar
+echo "--> exporting $image"
+#docker save "$image" -o "$name".tar
+#pigz -f "$name".tar
+
+docker save "$image" | pigz -c > "$name".tar.gz.tmp
+mv "$name".tar.gz.tmp "$name".tar.gz
+
 echo "--> saved to "$name".tar.gz"
 
 if [[ "$remove" == "true" ]]; then
