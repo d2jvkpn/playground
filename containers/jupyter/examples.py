3f2c0e0dd70de57f66b97bebe002cc72e6329a6a
d2jvkpn <d2jvkpn@noreply.local>
2025-10-13 11:40:10 +0800

    ...

--- containers/jupyter/examples.py
+++ containers/jupyter/examples.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+
+#### 1. torch, $ pip show torch
+import torch
+
+print(torch.cuda.is_available(), torch.version.cuda)
+
+
+#### 2. google colab
 import os
 from pathlib import Path
 
 from google.colab import userdata, drive
-
 
 os.environ['HF_USER'] = userdata.get('HF_USER')
 os.environ['HF_TOKEN'] = userdata.get('HF_TOKEN')
@@ -20,3 +27,14 @@ else:
 
 #$ ln -s /mnt/google_drive/MyDrive/data ./data
 #print(Path("data").glob("*"))
+
+#### 3. modal
+import os
+
+import modal
+
+app = modal.App("demo")
+
+@app.function(secrets=[modal.Secret.from_name("my-secret")])
+def run():
+    key = os.environ["OPENAI_API_KEY"]
--- containers/jupyter/run.sh
+++ containers/jupyter/run.sh
@@ -2,13 +2,13 @@
 set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 
 
-#### 1. 
+#### 1. load and init
 image=local/jupyter:pytorch2.8.0-cuda12.9-cudnn9-runtime
 py=python3.11
 
 mkdir -p configs data/pip logs
 
-#### 2. 
+#### 2. generate configs/container.env
 length=${1:-32}
 if [ -s "configs/container.env"  ]; then
     token=$(tr -dc '0-9a-z' < /dev/urandom | fold -w $length | head -n 1 || true)
@@ -16,7 +16,7 @@ if [ -s "configs/container.env"  ]; then
     echo "JUPYTER_TOKEN=$token" > configs/container.env
 fi
 
-#### 3. 
+#### 3. run
 docker run -d --restart=always --name=jupyter --hostname=jupyter \
   --network=host --gpus=all \
   -v $(pwd):/home/appuser/workspace \
@@ -27,6 +27,12 @@ docker run -d --restart=always --name=jupyter --hostname=jupyter \
   jupyter lab --no-browser --ip=0.0.0.0 --port=8888
 
 ####
+exit
+# -v $(pwd)/configs/pip.conf:/home/appuser/.config/pip/pip.conf
+# -v $(pwd)/configs/ipython_config.py:/home/appuser/.ipython/profile_default/ipython_config.py
+# -e PATH=/home/appuser/workspace/data/pip/bin:/opt/conda/bin:$PATH
+# -e PYTHONPATH=/home/appuser/workspace/data/pip:/opt/conda/lib/$(py)/site-packages
+
 exit
 conda install cuda-nvcc -c nvidia
 pip install flash-attn
