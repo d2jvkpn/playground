c5cdb2e153b1c8a241b3b1927c6ff2cdf9d2e7d4
d2jvkpn <d2jvkpn@noreply.local>
2025-11-05 17:29:17 +0800

    ...

--- ai/qwen/qwen3-vl.vllm.py
+++ ai/qwen/qwen3-vl.vllm.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
-import base64
+import os, base64
+from pathlib import Path
 
 from openai import OpenAI
 
 
-client = OpenAI(base_url="http://localhost:8000/v1", api_key="EMPTY")
+model_id = os.getenv("model_id", "Qwen/Qwen3-VL-2B-Instruct")
+vllm_addr = os.getenv("vllm_addr", "http://localhost:8000")
+
+client = OpenAI(base_url=f"{vllm_addr}/v1", api_key="EMPTY")
 system_prompt = "Answer concisely with only the final answer. No reasoning, no extra words."
 
-def image_to_uri(path):
-    mime = "image/png" if path.lower().endswith(".png") else "image/jpeg"
-    #with open(path, "rb") as f:
+def image_to_uri(p):
+    mime = "image/png" if p.lower().endswith(".png") else "image/jpeg"
+    #with open(p, "rb") as f:
     #    b64 = base64.b64encode(f.read()).decode("utf-8")
-    b64 = base64.b64encode(bts).decode("utf-8")
+    b64 = base64.b64encode(Path(p).read_bytes()).decode("utf-8")
 
     return f"data:{mime};base64,{b64}"
 
-img_uri = image_to_uri("data/images/candy.jpeg")
+
+image_url = image_to_uri("data/images/candy.jpeg")
 
 resp = client.chat.completions.create(
-    model="Qwen/Qwen3-VL-2B-Instruct",
+    model=model_id,
     stream=False,
     temperature=0.3,
-    max_tokens=256,
+    max_tokens=64,
     messages=[
         { "role": "system", "content": system_prompt },
         {
@@ -29,7 +34,7 @@ resp = client.chat.completions.create(
             "content": [
                 #{ "type": "text", "text": "What animal is on the candy?" },
                 { "type": "text", "text": "糖果上的是什么动物" },
-                { "type": "image_url", "image_url": { "url": img_uri } },
+                { "type": "image_url", "image_url": { "url": image_url } },
                 #{ "type": "image_url", "image_url": { "url": "https://example.com/street.jpg" } },
             ],
         },
