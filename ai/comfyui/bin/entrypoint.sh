cfaab2b6c8ea98aaaf33b884ee93b0d3b1197ca5
d2jvkpn <d2jvkpn@noreply.local>
2026-02-04 23:20:09 +0800

    ...

--- ai/comfyui/bin/entrypoint.sh
+++ ai/comfyui/bin/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eu
 
-if [ -s "/opt/init.sh" ]; then
-    echo "$(date +%FT%T%:z) ==> bash run: /opt/init.sh"
-    bash /opt/init.sh
-elif [ ! -z "$INIT_COMMAND" ]; then
-    echo "$(date +%FT%T%:z) ==> bash execute: $INIT_COMMAND"
-    bash -c "$INIT_COMMAND"
+if [ -s "/opt/container_init.sh" ]; then
+    echo "$(date +%FT%T%:z) ==> bash run: /opt/container_init.sh"
+    bash /opt/container_init.sh
+elif [ ! -z "$CONTAINER_INIT_COMMAND" ]; then
+    echo "$(date +%FT%T%:z) ==> bash execute: $CONTAINER_INIT_COMMAND"
+    bash -c "$CONTAINER_INIT_COMMAND"
 fi
 
 exec "$@"
