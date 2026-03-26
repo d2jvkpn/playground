5e313478b241e90260f23c41ad5e522619bd712f
d2jvkpn <d2jvkpn@noreply.local>
2026-03-27 00:44:47 +0800

    chromium_cdp.sh

--- ai/browsers/chromium-cdp.sh
+++ ai/browsers/chromium-cdp.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 args=$@
 cdp_port="${cdp_port:-9222}"
 cdp_bind_addr="${cdp_bind_addr:-172.17.0.1}"
-user_data_dir="${user_data_dir:-data/chromium}"
+user_data_dir="${user_data_dir:-data/chromium-cdp}"
 
 chromium_pid=""
 socat_pid=""
@@ -84,3 +84,38 @@ test with:
 EOF
 
 wait
+
+#### socat
+exit
+apt-get update
+
+apt-get install -y \
+  libnss3 \
+  libnss3-tools \
+  ca-certificates
+
+update-ca-certificates
+
+socat TCP-LISTEN:9222,fork,reuseaddr,bind=172.17.0.1 TCP:127.0.0.1:9222
+
+#### healthz check
+exit
+curl http://host.docker.internal:9222/json/version
+curl http://127.0.0.1:9222/json/version
+curl http://172.17.0.1:9222/json/version
+
+curl -X PUT "http://127.0.0.1:9222/json/new?https://www.baidu.com"
+
+
+#### docker
+exit
+docker run -d \
+  --name openclaw-cdp \
+  -p 9222:9222 \
+  ghcr.io/browserless/chromium \
+  chromium \
+    --headless=new \
+    --no-sandbox \
+    --remote-debugging-address=0.0.0.0 \
+    --remote-debugging-port=9222 \
+    --user-data-dir=/tmp/chromium
