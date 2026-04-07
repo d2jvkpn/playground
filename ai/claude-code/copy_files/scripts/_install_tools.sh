d6a7b4e65146a8a2ee1918245256e4c7bbeace90
d2jvkpn <d2jvkpn@noreply.local>
2026-04-07 23:30:16 +0800

    claude-code

---
+++ ai/claude-code/copy_files/scripts/_install_tools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
+
+
+bash /opt/scripts/apt_install.sh
+
+tag_name=$(
+  curl -fsSL https://api.github.com/repos/SagerNet/sing-box/releases/latest |
+  jq -r .tag_name
+)
+
+filename=sing-box-${tag_name#v}-linux-amd64.tar.gz
+
+curl -fL https://github.com/SagerNet/sing-box/releases/download/$tag_name/$filename -o $filename
+tar -xf $filename -C /opt/
+ln -s /opt/${filename%.tar.gz}/sing-box /usr/local/bin/
+
+rm -f $filename
