f694fdd9d53e857988c30ca4953e490db82a0cf4
d2jvkpn <d2jvkpn@noreply.local>
2026-04-13 11:37:22 +0800

    ...

--- ai/openclaw/copy_files/scripts/1000.openclaw-setup.sh
+++ ai/openclaw/copy_files/scripts/1000.openclaw-setup.sh
@@ -25,5 +25,5 @@ openclaw config set session.dmScope per-channel-peer
 auth=$(jq -n --arg token "$(openssl rand -hex 32)" '{mode:"token", token:$token}')
 openclaw config set gateway.auth "$auth" --strict-json
 
-allowed_origins='["http://localhost:18789","http://127.0.0.1:18789"]'
+allowed_origins='["http://localhost:18789","http://127.0.0.1:18789","http://127.0.0.1:18790"]'
 openclaw config set gateway.controlUi.allowedOrigins "$allowed_origins" --strict-json
--- linux/shell/docker_skopeo.sh
+++ linux/shell/docker_skopeo.sh
@@ -3,11 +3,18 @@ set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 
 
 output_dir=${output_dir:-./}
-image=$1
-prefix=${output_dir%/}/$(echo "$image" | sed 's#/#--#g; s#:#--#')
+name=$1
+prefix=${output_dir%/}/$(echo "$name" | sed 's#/#--#g; s#:#--#')
 
-skopeo copy docker://docker.io/$image \
-  docker-archive:/dev/stdout:$image |
+first="${name%%/*}"
+if [[ "$first" == "localhost" || "$first" == *.* || "$first" == *:* ]]; then
+    image=$name
+else
+    image=docker.io/$name
+fi
+
+
+skopeo copy docker://$image docker-archive:/dev/stdout:$image |
   pigz -c > $prefix.tgz.tmp
 
 mv $prefix.tgz.tmp $prefix.tgz
