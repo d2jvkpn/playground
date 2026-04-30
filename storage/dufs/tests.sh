67347f4d46dc60c851c97ef5b169f9cdc51f94d4
d2jvkpn <d2jvkpn@noreply.local>
2026-04-30 14:04:22 +0800

    ...

--- storage/dufs/tests.sh
+++ storage/dufs/tests.sh
@@ -2,15 +2,25 @@
 set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 
 
+####
 url=$(yq .dufs.url configs/access.yaml)
 username=$(yq .dufs.username configs/access.yaml)
 password=$(yq .dufs.password configs/access.yaml)
 
-
+####
 date +'%F%T%:z' > data/test.txt
 
 curl -i -u "$username:$password" -T ./data/test.txt $url/test.txt  # 201
+curl -i -u "$username:$password" -T ./data/test.txt $url/test.txt  # 403
 
 curl -i $url/test.txt                                              # 200
 
 curl -i -T ./data/test.txt $url/anonymous-test.txt                 # 401
+
+####
+token=$(printf "$username:$password" | base64 -w 0)
+
+curl -i -X PUT \
+  -H "Authorization: Basic $token" \
+  -H "Content-Type: text/plain" \
+  -d @./data/test.txt $url/hello.txt
